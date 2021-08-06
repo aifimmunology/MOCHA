b66dc3505ff177ba593e0d4b050a91dac57900db
SamirRachidZaim-AIFI <samir.rachidzaim@alleninstitute.org>
2021-08-05 18:20:29 -0700

    initial commit

--- R/calculate_intensities.R
+++ R/calculate_intensities.R
@@ -1,18 +1,116 @@
-# Hello, world!
-#
-# This is an example function named 'hello'
-# which prints 'Hello, world!'.
-#
-# You can learn more about package authoring with RStudio at:
-#
-#   http://r-pkgs.had.co.nz/
-#
-# Some useful keyboard shortcuts for package authoring:
-#
-#   Install Package:           'Cmd + Shift + B'
-#   Check Package:             'Cmd + Shift + E'
-#   Test Package:              'Cmd + Shift + T'
+#' @title \code{calculate_intensities}
+#'
+#' @description \code{calculate_intensities} is an R helper function, part of the single-cell peak calling
+#' algorithm scMACS by (XXX et al, 2021) that calculates the design matrix, X,
+#' used to make peak calls.
+#'
+#' @param fragMat a genomic ranges object containing the fragments
+#' @param PeakRegion a genomic ranges object indicating where to call peaks. This could be a pre-selected peak set or a dynamic bins approach to searching the whole genome.
+#' @param normalizeBins a boolean indicating whether varying bin widths should be normalized to a fixed width
+#'
+#' @return a data.table, countsByBin, that returns the two intensity parameters required
+#' to calculate the probability of a (+) peak
+#'
+#' @details The technical details of the algorithm are found in XX.
+#'
+#' @references XX
+#'
 
-calculate_intensities <- function() {
+
+calculate_intensities <- function(fragMat,
+                                  positivePeaks,
+                                  normalizeBins=TRUE) {
   print("calculating intensities from ArchR Project!")
+
+  fragMat_dt <- as.data.table(fragMat)
+
+  ## transform granges to data.table
+  positivePeaksDF <- as.data.table(positivePeaks)
+  positivePeaksDF$bin <- paste(positivePeaksDF$seqnames, ':',positivePeaksDF$start,
+                               '-',positivePeaksDF$end,sep='')
+
+  ###
+  fragsPerBin <- findOverlaps(fragMat,
+                              positivePeaks,
+                              minoverlap=0)
+
+  fragsPerBin <- as.data.table(fragsPerBin)
+
+  numCells = length(unique(fragMat_dt$RG))
+
+  # 		fragsPerBin <- fragsPerBin[order(fragsPerBin$subjectHits),]
+  fragsPerBin$cell <- fragMat_dt$RG[fragsPerBin$queryHits]
+
+  fragsPerBin$bin <-  positivePeaksDF$bin[fragsPerBin$subjectHits]
+  fragsPerBin$chr <-  positivePeaksDF$seqnames[fragsPerBin$subjectHits]
+  fragsPerBin$start <- positivePeaksDF$start[fragsPerBin$subjectHits]
+  fragsPerBin$width <- positivePeaksDF$width[fragsPerBin$subjectHits]
+
+
+  if(normalizeBins){
+    ### get cell count matrix
+    cell_counts = fragsPerBin[,list(N=.N,
+                                    width=unique(width)),
+                              by=list(bin,cell)
+    ]
+    cell_counts$normed_width <- pmax(1, round(cell_counts$width/500))
+    cell_counts$normed_counts <- pmax(1,round(cell_counts$N/cell_counts$normed_width))
+
+    #### doing bin-level summaries
+    setkey(cell_counts, bin)
+    cat('\n\nCalculating Region-Normalized reads per cell using 500bp wide bins\n\n')
+
+    countsByBin <-  cell_counts[, list(CellsWithNoReads=(numCells-sum(N>=1))/numCells,
+                                       ReadsPerCell=sum(N)/numCells,
+                                       maxIntensity = max(N)
+    ), by=bin
+    ]
+
+  } else{
+    ### get cell count matrix
+    cell_counts = fragsPerBin[,list(N=.N),
+                              by=list(bin,cell)
+    ]
+
+    #### doing bin-level summaries
+    setkey(cell_counts, bin)
+
+    #### calculate features
+    countsByBin <-  cell_counts[, list(CellsWithNoReads=(numCells-sum(N>=1))/numCells,
+                                       ReadsPerCell=sum(N)/numCells,
+                                       maxIntensity = max(N)
+    ), by=bin
+    ]
+  }
+
+  countsByBin <- dplyr::left_join(positivePeaksDF[, c('seqnames','start','bin','width')],
+                                  countsByBin,
+                                  by='bin')
+
+  countsByBin <- countsByBin[order(countsByBin$seqnames, countsByBin$start, decreasing=FALSE),]
+
+  ## creating counts for bins with no reads
+  countsByBin$CellsWithNoReads[is.na(countsByBin$CellsWithNoReads)] <- 1
+  countsByBin$ReadsPerCell[is.na(countsByBin$ReadsPerCell)] <- 0
+  countsByBin$maxIntensity[is.na(countsByBin$maxIntensity)] <- 0
+
+  countsByBin$threeBin <- rollapply(countsByBin$ReadsPerCell, FUN=mean, width=3, by=1 ,partial=TRUE, alight='left')
+  countsByBin$numCells <- numCells
+
+
+  chromosomeOrder <- c("chr1"  ,"chr2" , "chr3",  "chr4",
+                       "chr5" , "chr6" , "chr7"  ,"chr8",
+                       "chr9" ,"chr10" ,"chr11", "chr12",
+                       "chr13", "chr14", "chr15", "chr16",
+                       "chr17", "chr18" ,"chr19", "chr20",
+                       "chr21", "chr22", "chrX", 'chrY' )
+
+  countsByBin$chromosome <- factor(countsByBin$seqnames, levels=chromosomeOrder, ordered=T)
+  countsByBin <- countsByBin[order(countsByBin$chromosome, countsByBin$start, decreasing=FALSE),]
+
+  countsByBin$threeBin <- rollapply(countsByBin$ReadsPerCell, FUN=mean, width=3, by=1 ,partial=TRUE, alight='left')
+
+
+  print(paste('Analysis finished on ', numCells, 'cells'))
+  return(countsByBin)
 }
---
+++ R/determine_dynamic_range.R
@@ -0,0 +1,48 @@
+#' @title \code{determine_dynamic_range}
+#'
+#' @description \code{determine_dynamic_range} is an R helper function, part of the single-cell peak calling
+#' algorithm scMACS by (XXX et al, 2021) that determines which genomic regions, or bins,
+#' will be used for de-novo peak calling. The function "determine_dynamic_range" is a wrapper function
+#' around the dynamic_bins function
+#'
+#'
+#' @param AllFragmentsList: List of fragments by arrow file
+#' @param GeneralWindowSize: Window size for sliding window generated over longer fragments.
+#' @param WindowSizeRange: The sliding window function will generate a smaller window at the end of a longer fragment,
+#if the fragment is not evenly divisible by GeneralWindowSize. If that smaller window is less than or equal to
+#WindowSizeRange, then it will be merged with the preceding window to great a larger window. This means that
+#longer fragments will be broken up into bins that are between WindowSizeRange and WindowSizeRange+GeneralWindowSize in length.
+#' @param coreNum an integer indicating the number of cores to use
+#' @param doBins is a boolean variable. When true, then it will into windows according to GeneralWindowSize and WindowSizeRange.
+
+#' @return a data.table, countsByBin, that returns the two intensity parameters required
+#' to calculate the probability of a (+) peak
+#'
+#' @details The technical details of the algorithm are found in XX.
+#'
+#' @references XX
+#'
+
+
+determine_dynamic_range <- function(AllFragmentsList, cellSubsetArchR, binSize, doBin){
+  TotalRange <- DynamicBins(AllFragmentsList = AllFragmentsList,
+                            doBin = FALSE,
+                            coreNum = 30)
+
+  blackList <- getBlacklist(cellSubsetArchR)
+
+  #Let's subtract out areas of fragments that overlap with blacklist regions.
+  #Let's not remove the region entirely, because the regions may be long or only
+
+  TotalRangesFilt<- setdiff_ranges(TotalRange, blackList)
+
+  RangeBins <- plyranges::stretch(anchor_end(TotalRangesFilt),
+                                  extend = start(TotalRangesFilt)%% binSize)
+
+  FinalBins <- stretch(anchor_start(RangeBins),extend = (binSize - end(RangeBins)%% binSize)) %>%
+    reduce_ranges() %>% slide_ranges(width = binSize, step = binSize)%>% filter(width(.) ==500)
+
+  return(FinalBins)
+
+  print('fragments loaded')
+}
