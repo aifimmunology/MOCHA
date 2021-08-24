080f62c44f18362698d9f9124737cd8257e16a68
SamirRachidZaim-AIFI <samir.rachidzaim@alleninstitute.org>
2021-08-24 14:45:33 -0700

    updating

--- R/determine_dynamic_range.R
+++ R/determine_dynamic_range.R
@@ -24,12 +24,34 @@
 #'
 
 
-determine_dynamic_range <- function(AllFragmentsList, cellSubsetArchR, binSize, doBin){
+determine_dynamic_range <- function(AllFragmentsList, ArchRProject, binSize, doBin){
   TotalRange <- DynamicBins(AllFragmentsList = AllFragmentsList,
                             doBin = FALSE,
                             coreNum = 30)
 
-  blackList <- getBlacklist(cellSubsetArchR)
+  if(class(AllFragmentsList)!='list'){
+    stop('AllFragmentsList must be a list of arrow files')
+  }
+
+  if(class(ArchRProject)!='ArchRProject'){
+    stop('ArchRProject must be an ArchR Project')
+  }
+
+  if(class(binSize)!='numeric' | binSize <0){
+    stop(paste('invalid binSize!: binSize must be an integer value > 0 indicating the width of the genomic region check "binSize=',
+               binSize,'" input', sep=''))
+
+  }
+
+  if(binSize > 5000){
+    warning('binSize is > 5,000bp. Do you want to set the tile size smaller for peakcalling?')
+  }
+
+  if(class(doBin) != 'logical'){
+    stop('doBin user-input must be a TRUE/FALSE boolean input')
+  }
+
+  blackList <- getBlacklist(ArchRProject)
 
   #Let's subtract out areas of fragments that overlap with blacklist regions.
   #Let's not remove the region entirely, because the regions may be long or only
--- R/make_prediction.R
+++ R/make_prediction.R
@@ -6,8 +6,10 @@
 #' logistic regression model predictions
 #'
 #'
-#' @param intensityMatrix: List of fragments by arrow file
-
+#' @param X an intensityMatrix output from \code{calculate_intensities}
+#' @param finalModel is a matrix with coefficients and an index indicating
+#'        the number of cell used to train that model
+#'
 #' @return the original intensityMatrix with the two intensity parameters required
 #' to calculate the probability of a (+) peak, with an additional two columns
 #' that include the prediction probability
@@ -18,47 +20,21 @@
 #'
 
 
-make_prediction <- function(intensityMatrix){
-  TotalRange <- DynamicBins(AllFragmentsList = AllFragmentsList,
-                            doBin = FALSE,
-                            coreNum = 30)
+make_prediction <- function(X, finalModel){
 
-  if(class(AllFragmentsList)!='list'){
-    stop('AllFragmentsList must be a list of arrow files')
+    cell_model = X$numCells[1]
+    X = cbind(rep(1, nrow(X)),
+              X[,c('lambda1','lambda2')]
+    )
+
+    model.idx <- which.min(abs(cell_model - finalModel$numCells))
+
+    tmpModel <- finalModel[model.idx,2:4]
+    tmpModel = as.matrix(tmpModel)
+    tmpModel = t(tmpModel)
+    X = as.matrix(X)
+    preds = X%*% tmpModel
+    preds = 1/(1+exp(-preds))
+    return(preds)
+
   }
-
-  if(class(ArchRProject)!='ArchRProject'){
-    stop('ArchRProject must be an ArchR Project')
-  }
-
-  if(class(binSize)!='numeric' | binSize <0){
-    stop(paste('invalid binSize!: binSize must be an integer value > 0 indicating the width of the genomic region check "binSize=',
-               binSize,'" input', sep=''))
-
-  }
-
-  if(binSize > 5000){
-    warning('binSize is > 5,000bp. Do you want to set the tile size smaller for peakcalling?')
-  }
-
-  if(class(doBin) != 'logical'){
-    stop('doBin user-input must be a TRUE/FALSE boolean input')
-  }
-
-  blackList <- getBlacklist(ArchRProject)
-
-  #Let's subtract out areas of fragments that overlap with blacklist regions.
-  #Let's not remove the region entirely, because the regions may be long or only
-
-  TotalRangesFilt<- setdiff_ranges(TotalRange, blackList)
-
-  RangeBins <- plyranges::stretch(anchor_end(TotalRangesFilt),
-                                  extend = start(TotalRangesFilt)%% binSize)
-
-  FinalBins <- stretch(anchor_start(RangeBins),extend = (binSize - end(RangeBins)%% binSize)) %>%
-    reduce_ranges() %>% slide_ranges(width = binSize, step = binSize)%>% filter(width(.) ==binSize)
-
-  return(FinalBins)
-
-  print('fragments loaded')
-}
