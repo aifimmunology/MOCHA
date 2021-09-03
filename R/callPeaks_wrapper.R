8bbe2c683696fc886089e1e7e9c90820264ca10b
markphillippebworth <73675546+markphillippebworth@users.noreply.github.com>
2021-09-03 14:06:27 -0700

    Created wrapper for calling peaks

---
+++ R/callPeaks_wrapper.R
@@ -0,0 +1,18 @@
+
+
+require(plyranges)
+require(GenomicRanges)
+require(ArchR)
+
+
+
+
+
+
+candidatePeaks <- scMACS::determine_dynamic_range(sampleFrags, cellSubsetArchR,500,doBin=FALSE)
+int_matrix_1 <- scMACS::calculate_intensities(fragMat = sampleFrags[[1]],candidatePeaks=candidatePeaks,
+			NBDistribution = NBDistribution_global,normalizeBins=FALSE)
+
+int_matrix_2 <- scMACS::calculate_intensities(fragMat = sampleFrags[[2]],candidatePeaks=candidatePeaks,
+			NBDistribution = NBDistribution_global,normalizeBins=FALSE)
+
