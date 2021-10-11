0e9b9b59a5ff39f18cee0a955439da9b958e9130
SamirRachidZaim-AIFI <samir.rachidzaim@alleninstitute.org>
2021-10-11 21:50:55 +0000

    updating documentation files

---
+++ R/.ipynb_checkpoints/dynamic_bins-checkpoint.R
@@ -0,0 +1,52 @@
+#' @title \code{dynamic_bins}
+#'
+#' @description \code{dynamic_bins} is an R helper function, part of the single-cell peak calling
+#' algorithm scMACS by (XXX et al, 2021) that determines which genomic regions, or bins,
+#' will be used for de-novo peak calling. The function "dynamic_bins" generates bins for peak calling based on the actual fragments present within a sample.
+#AllFragmentsList: List of fragments by arrow file
+#'
+#'
+#' @param AllFragmentsList: List of fragments by arrow file
+#' @param GeneralWindowSize: Window size for sliding window generated over longer fragments.
+#' @param WindowSizeRange: The sliding window function will generate a smaller window at the end of a longer fragment,
+#if the fragment is not evenly divisible by GeneralWindowSize. If that smaller window is less than or equal to
+#WindowSizeRange, then it will be merged with the preceding window to great a larger window. This means that
+#longer fragments will be broken up into bins that are between WindowSizeRange and WindowSizeRange+GeneralWindowSize in length.
+#' @param coreNum an integer indicating the number of cores to use
+#' @param doBins is a boolean variable. When true, then it will into windows according to GeneralWindowSize and WindowSizeRange.
+
+#' @return a data.table, countsByBin, that returns the two intensity parameters required
+#' to calculate the probability of a (+) peak
+#'
+#' @details The technical details of the algorithm are found in XX.
+#'
+#' @references XX
+#'
+#' @export
+
+
+dynamic_bins <- function(AllFragmentsList, GeneralWindowSize, WindowSizeRange, coreNum, doBin){
+
+  AllFragsList <- mclapply(AllFragmentsList, function(x){
+    reduce_ranges(x, counts = n())
+  }, mc.cores = coreNum)
+  AllFrags <- bind_ranges(AllFragsList) %>% reduce_ranges(counts = sum(counts))
+
+  if(doBin){
+    print("01 - Fragments bound into one set of non-overlapping ranges")
+
+    SmallFrags <- AllFrags[width(AllFrags) <= GeneralWindowSize + WindowSizeRange]
+
+    BigFrags <- AllFrags[width(AllFrags) > GeneralWindowSize + WindowSizeRange] %>%
+      slide_ranges(width = GeneralWindowSize, step = GeneralWindowSize) %>%
+      anchor_end() %>% mutate(partition = (as.character(partition))) %>%
+      mutate(width = ifelse(width <= WindowSizeRange,width + 2,width-1)) %>% group_by(partition) %>%
+      reduce_ranges(minoverlap = 2) %>% mutate(width = width +1)
+    print("02 - Longer fragments broken into sliding windows")
+    FinalBins <- bind_ranges(BigFrags, SmallFrags)
+  }else{
+    FinalBins <- AllFrags
+  }
+
+  return(FinalBins)
+}
