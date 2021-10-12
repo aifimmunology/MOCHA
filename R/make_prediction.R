2c6d3b99451b6697331e03308c0030f105f0d1c7
SamirRachidZaim-AIFI <samir.rachidzaim@alleninstitute.org>
2021-10-12 17:38:53 +0000

    changed 'error' to return NULL for samples < 5 cells

--- R/make_prediction.R
+++ R/make_prediction.R
@@ -25,66 +25,69 @@ make_prediction <- function(X, finalModelObject){
     cell_model = X$numCells[1]
     
     if(cell_model  < 5){
-        stop('Cannot make peak calls with < 5 cells')
+        print('Cannot make peak calls with < 5 cells, returning NULL object')
+        return(NULL)
+    } else {
+    
+
+       if(cell_model<=80000){
+
+           lambda0 <- predict(finalModelObject$LogFits[[1]], data.frame(numCells=cell_model))
+           lambda1 <- predict(finalModelObject$LogFits[[2]], data.frame(numCells=cell_model))
+           lambda2 <- predict(finalModelObject$LogFits[[3]], data.frame(numCells=cell_model))
+           tmpModel = c(lambda0, lambda1, lambda2    )
+           tmpModel = as.matrix(tmpModel)
+
+
+       } else if(cell_model>80000 & cell_model <= 120000){ 
+
+
+           lambda0 <- mean(predict(finalModelObject$LogFits[[1]], data.frame(numCells=cell_model)),
+                           predict(finalModelObject$LinearFits[[1]], data.frame(numCells=cell_model))
+                           )
+           lambda1 <- mean(predict(finalModelObject$LogFits[[2]], data.frame(numCells=cell_model)),
+                           finalModelObject$LinearFits[[2]]
+                           )
+           lambda0 <- mean(predict(finalModelObject$LogFits[[3]], data.frame(numCells=cell_model)),
+                           predict(finalModelObject$LinearFits[[3]], data.frame(numCells=cell_model))
+                           )       
+
+           tmpModel = c(lambda0, lambda1, lambda2    )
+           tmpModel = as.matrix(tmpModel)
+
+
+
+       } else {
+
+           lambda0 <- predict(finalModelObject$LinearFits[[1]], data.frame(numCells=cell_model))
+           lambda1 <- finalModelObject$LinearFits[[2]]
+           lambda2 <- predict(finalModelObject$LinearFits[[3]], data.frame(numCells=cell_model))
+
+           tmpModel = c(lambda0, lambda1, lambda2    )
+           tmpModel = as.matrix(tmpModel)    
+
+        }
+
+
+       noiseZ <- c(1,min(X$lambda1), min(X$lambda2)) %*% tmpModel
+       adaptiveThreshold <- max(0.5, 1/(1+exp(-noiseZ)))
+
+
+       designX <- X[,c('lambda1','lambda2')]
+       designX$Intercept =1 
+       designX <- designX[,c('Intercept','lambda1','lambda2')]       
+
+       z = as.matrix(designX) %*% tmpModel
+       preds = 1/(1+exp(-z))
+
+       X$Prediction = preds 
+       X =makeGRangesFromDataFrame(X, keep.extra.columns=T)
+       X$PredictionStrength = X$lambda1
+       X$Peak = X$Prediction > adaptiveThreshold
+
+        return(X)
+
+
+        
     }
-    
-
-   if(cell_model<=80000){
-       
-       lambda0 <- predict(finalModelObject$LogFits[[1]], data.frame(numCells=cell_model))
-       lambda1 <- predict(finalModelObject$LogFits[[2]], data.frame(numCells=cell_model))
-       lambda2 <- predict(finalModelObject$LogFits[[3]], data.frame(numCells=cell_model))
-       tmpModel = c(lambda0, lambda1, lambda2    )
-       tmpModel = as.matrix(tmpModel)
-       
-       
-   } else if(cell_model>80000 & cell_model <= 120000){ 
-    
-         
-       lambda0 <- mean(predict(finalModelObject$LogFits[[1]], data.frame(numCells=cell_model)),
-                       predict(finalModelObject$LinearFits[[1]], data.frame(numCells=cell_model))
-                       )
-       lambda1 <- mean(predict(finalModelObject$LogFits[[2]], data.frame(numCells=cell_model)),
-                       finalModelObject$LinearFits[[2]]
-                       )
-       lambda0 <- mean(predict(finalModelObject$LogFits[[3]], data.frame(numCells=cell_model)),
-                       predict(finalModelObject$LinearFits[[3]], data.frame(numCells=cell_model))
-                       )       
-
-       tmpModel = c(lambda0, lambda1, lambda2    )
-       tmpModel = as.matrix(tmpModel)
-       
-       
-      
-   } else {
-             
-       lambda0 <- predict(finalModelObject$LinearFits[[1]], data.frame(numCells=cell_model))
-       lambda1 <- finalModelObject$LinearFits[[2]]
-       lambda2 <- predict(finalModelObject$LinearFits[[3]], data.frame(numCells=cell_model))
-       
-       tmpModel = c(lambda0, lambda1, lambda2    )
-       tmpModel = as.matrix(tmpModel)    
-       
-    }
-
-    
-   noiseZ <- c(1,min(X$lambda1), min(X$lambda2)) %*% tmpModel
-   adaptiveThreshold <- max(0.5, 1/(1+exp(-noiseZ)))
-
-
-   designX <- X[,c('lambda1','lambda2')]
-   designX$Intercept =1 
-   designX <- designX[,c('Intercept','lambda1','lambda2')]       
-
-   z = as.matrix(designX) %*% tmpModel
-   preds = 1/(1+exp(-z))
-
-   X$Prediction = preds 
-   X =makeGRangesFromDataFrame(X, keep.extra.columns=T)
-   X$PredictionStrength = X$lambda1
-   X$Peak = X$Prediction > adaptiveThreshold
-       
-    return(X)
-    
-
 }
