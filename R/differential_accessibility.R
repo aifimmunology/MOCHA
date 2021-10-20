260119a24b1c491ac6dab9ad1e34925f4b26aeac
SamirRachidZaim-AIFI <samir.rachidzaim@alleninstitute.org>
2021-10-20 00:13:25 +0000

    making sure NAs and 0s are distinguished in hypothesis test

--- R/differential_accessibility.R
+++ R/differential_accessibility.R
@@ -24,97 +24,120 @@ differential_accessibility <- function(groupA, groupB, candidatePeak='chr1:81700
     sample_names_grB <- names(groupB[['scMACS_peaks']])
     sample_names_grB <- sample_names_grB[sample_names_grB != 'Union']
     
-    numCellsA <- sapply(groupA[['scMACS_peaks']][sample_names_grA],
-                        function(x) x$numCells[1]
-                        )
-    
-    numCellsB <- sapply(groupB[['scMACS_peaks']][sample_names_grB],
-                        function(x) x$numCells[1]
-                        )
-    
-    
-    vals_groupA <- as.numeric(sapply(groupA[['scMACS_peaks']][sample_names_grA],
-                        function(x) x$lambda1[x$PeakID ==candidatePeak]
-                        )
-                              )
-    
-    vals_groupB <- as.numeric(sapply(groupB[['scMACS_peaks']][sample_names_grB],
-                        function(x)  x$lambda1[x$PeakID ==candidatePeak]
-                        ) 
-                              )
-    
-    FinalBins_groupA <- determine_dynamic_range(SimpleList(groupA$SampleFragments),
-                                         ArchRProj, 
-                                         binSize=500, 
-                                         doBin=FALSE)
-    
-    FinalBins_groupB <- determine_dynamic_range(SimpleList(groupB$SampleFragments),
-                                         ArchRProj, 
-                                         binSize=500, 
-                                         doBin=FALSE)
-
-
-    ## by-default 
-    vals_groupB[is.na(vals_groupB)] <- 0 
-    vals_groupA[is.na(vals_groupA)] <- 0 
-
-    
-     get_lambda1_subsample <- function(sampleSize, ind_sample, candidatePeak, FinalBins){
+    if(length(sample_names_grB) < 2 | length(sample_names_grA) <2){
+        
+        print('not enough samples with at least 5 cells to determine differential accessibility')
          
-            cellList <- unique(ind_sample$RG)
-            subsample <- ind_sample[ind_sample$RG %in% sample(cellList,sampleSize, replace=F)]
+        res = data.frame(
+            Peak=candidatePeak,
+            ES=NA,
+            P.value=NA,
+            PDR_A=NA,
+            L1A_avg=NA,
+            PDR_B= NA,
+            L1B_avg=NA
+        )
+     
+        return(res)
+        
+    } else {
+    
+        numCellsA <- sapply(groupA[['scMACS_peaks']][sample_names_grA],
+                            function(x) x$numCells[1]
+                            )
 
-            tmp <- scMACS::calculate_intensities(subsample,
-                                          FinalBins,
-                                          NBDistribution=NULL,
-                                          normalizeBins=FALSE,
-                                          theta=0.001, 
-                                          width=20
-                                         )
-            tmpL1 <- round( tmp$lambda1[tmp$PeakID ==candidatePeak],4)
-            print(tmpL1)
-            return(tmpL1)
+        numCellsB <- sapply(groupB[['scMACS_peaks']][sample_names_grB],
+                            function(x) x$numCells[1]
+                            )
 
+
+        vals_groupA <- as.numeric(sapply(groupA[['scMACS_peaks']][sample_names_grA],
+                            function(x) x$lambda1[x$PeakID ==candidatePeak]
+                            )
+                                  )
+
+        vals_groupB <- as.numeric(sapply(groupB[['scMACS_peaks']][sample_names_grB],
+                            function(x)  x$lambda1[x$PeakID ==candidatePeak]
+                            ) 
+                                  )
+
+        FinalBins_groupA <- determine_dynamic_range(SimpleList(groupA$SampleFragments),
+                                             ArchRProj, 
+                                             binSize=500, 
+                                             doBin=FALSE)
+
+        FinalBins_groupB <- determine_dynamic_range(SimpleList(groupB$SampleFragments),
+                                             ArchRProj, 
+                                             binSize=500, 
+                                             doBin=FALSE)
+
+
+        ## If a sample had > 5 cells but no reads,
+        ## lambda1 is designated as 0. So here 
+        ## we change the NAs (R-artifact) to 0s 
+        vals_groupB[is.na(vals_groupB)] <- 0 
+        vals_groupA[is.na(vals_groupA)] <- 0 
+
+
+         get_lambda1_subsample <- function(sampleSize, ind_sample, candidatePeak, FinalBins){
+
+                cellList <- unique(ind_sample$RG)
+                subsample <- ind_sample[ind_sample$RG %in% sample(cellList,sampleSize, replace=F)]
+
+                tmp <- scMACS::calculate_intensities(subsample,
+                                              FinalBins,
+                                              NBDistribution=NULL,
+                                              normalizeBins=FALSE,
+                                              theta=0.001, 
+                                              width=20
+                                             )
+                tmpL1 <- round( tmp$lambda1[tmp$PeakID ==candidatePeak],4)
+                print(tmpL1)
+                return(tmpL1)
+
+            }
+
+
+
+        calculate_PDR <- function(groupB, numCellsB, vals_groupB, candidatePeak, FinalBins){
+
+                min_num_sample <- min(numCellsB)
+                most_open_sample <-  which.max(vals_groupB)
+
+                print(paste("Smallest sample has cells =", min_num_sample))
+
+                ind_sample <- groupB$SampleFragments[[most_open_sample]]
+
+                lambda1_subsamples <- unlist(mclapply(1:25, function(x) 
+                    try(get_lambda1_subsample(min_num_sample, ind_sample,candidatePeak,FinalBins )),
+                         mc.cores=numCores,
+                         mc.preschedule=TRUE,
+                         mc.allow.recursive=FALSE
+                         ))
+
+                pdr = mean(lambda1_subsamples > 0)
+                return( pdr)
         }
 
-     
-    
-    calculate_PDR <- function(groupB, numCellsB, vals_groupB, candidatePeak, FinalBins){
+
+        pdrA = calculate_PDR(groupA, numCellsA, vals_groupA ,candidatePeak, FinalBins_groupA)
+        pdrB = calculate_PDR(groupB, numCellsB, vals_groupB,candidatePeak, FinalBins_groupB)    
+
+
+        res = data.frame(
+            Peak=candidatePeak,
+            ES=wilcox.test(vals_groupA, vals_groupB)$statistic,
+            P.value=wilcox.test(vals_groupA, vals_groupB)$p.value,
+            PDR_A=pdrA,
+            L1A_avg=mean(vals_groupA),
+            PDR_B= pdrB,
+            L1B_avg=mean(vals_groupB)
+            )
+
+        return(res)
         
-            min_num_sample <- min(numCellsB)
-            most_open_sample <-  which.max(vals_groupB)
-
-            print(paste("Smallest sample has cells =", min_num_sample))
-
-            ind_sample <- groupB$SampleFragments[[most_open_sample]]
-
-            lambda1_subsamples <- unlist(mclapply(1:10, function(x) 
-                try(get_lambda1_subsample(min_num_sample, ind_sample,candidatePeak,FinalBins )),
-                     mc.cores=numCores,
-                     mc.preschedule=TRUE,
-                     mc.allow.recursive=FALSE
-                     ))
-
-            pdr = mean(lambda1_subsamples > 0)
-            return( pdr)
+        
     }
-    
-            
-    pdrA = calculate_PDR(groupA, numCellsA, vals_groupA ,candidatePeak, FinalBins_groupA)
-    pdrB = calculate_PDR(groupB, numCellsB, vals_groupB,candidatePeak, FinalBins_groupB)    
-        
-    
-    res = data.frame(
-        Peak=candidatePeak,
-        ES=wilcox.test(vals_groupA, vals_groupB)$statistic,
-        P.value=wilcox.test(vals_groupA, vals_groupB)$p.value,
-        PDR_A=pdrA,
-        L1_avg=mean(vals_groupA),
-        PDR_B= pdrB,
-        L2_avg=mean(vals_groupB)
-        )
-    
-    return(res)
-    
+
 }
 
