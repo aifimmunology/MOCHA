e4911391087182e4b811f2624d8538013ef35b82
SamirRachidZaim-AIFI <samir.rachidzaim@alleninstitute.org>
2021-10-21 20:32:21 +0000

    added chi-square test

--- R/differential_accessibility.R
+++ R/differential_accessibility.R
@@ -15,7 +15,7 @@
 #' @references XX
 #'
 
-differential_accessibility <- function(groupA, groupB, candidatePeak='chr1:817000-817499' ){
+differential_accessibility <- function(groupA, groupB, candidatePeaks='chr1:817000-817499', doPDRAnalysis=FALSE  ){
     
     
     sample_names_grA <- names(groupA[['scMACS_peaks']])
@@ -27,16 +27,16 @@ differential_accessibility <- function(groupA, groupB, candidatePeak='chr1:81700
     if(length(sample_names_grB) < 2 | length(sample_names_grA) <2){
         
         print('not enough samples with at least 5 cells to determine differential accessibility')
-         
-        res = data.frame(
-            Peak=candidatePeak,
-            ES=NA,
-            P.value=NA,
-            PDR_A=NA,
-            L1A_avg=NA,
-            PDR_B= NA,
-            L1B_avg=NA
-        )
+        
+        res=data.frame(Peak = candidatePeaks,
+                       ES_wilc=rep(NA, length(candidatePeaks)),
+                       Wilcoxon=rep(NA, length(candidatePeaks)),
+                       ES_Chisq=rep(NA, length(candidatePeaks)),
+                       Chisquare = rep(NA, length(candidatePeaks)),
+                       MinPval = rep(NA, length(candidatePeaks)),
+                       L1A_avg=rep(NA, length(candidatePeaks)),
+                       L1B_avg=rep(NA, length(candidatePeaks))
+                                )
      
         return(res)
         
@@ -51,35 +51,9 @@ differential_accessibility <- function(groupA, groupB, candidatePeak='chr1:81700
                             )
 
 
-        vals_groupA <- as.numeric(sapply(groupA[['scMACS_peaks']][sample_names_grA],
-                            function(x) x$lambda1[x$PeakID ==candidatePeak]
-                            )
-                                  )
 
-        vals_groupB <- as.numeric(sapply(groupB[['scMACS_peaks']][sample_names_grB],
-                            function(x)  x$lambda1[x$PeakID ==candidatePeak]
-                            ) 
-                                  )
-
-        FinalBins_groupA <- determine_dynamic_range(SimpleList(groupA$SampleFragments),
-                                             ArchRProj, 
-                                             binSize=500, 
-                                             doBin=FALSE)
-
-        FinalBins_groupB <- determine_dynamic_range(SimpleList(groupB$SampleFragments),
-                                             ArchRProj, 
-                                             binSize=500, 
-                                             doBin=FALSE)
-
-
-        ## If a sample had > 5 cells but no reads,
-        ## lambda1 is designated as 0. So here 
-        ## we change the NAs (R-artifact) to 0s 
-        vals_groupB[is.na(vals_groupB)] <- 0 
-        vals_groupA[is.na(vals_groupA)] <- 0 
-
-
-         get_lambda1_subsample <- function(sampleSize, ind_sample, candidatePeak, FinalBins){
+        
+        get_lambda1_subsample <- function(sampleSize, ind_sample, candidatePeak, FinalBins){
 
                 cellList <- unique(ind_sample$RG)
                 subsample <- ind_sample[ind_sample$RG %in% sample(cellList,sampleSize, replace=F)]
@@ -95,7 +69,7 @@ differential_accessibility <- function(groupA, groupB, candidatePeak='chr1:81700
                 print(tmpL1)
                 return(tmpL1)
 
-            }
+        }
 
 
 
@@ -119,23 +93,135 @@ differential_accessibility <- function(groupA, groupB, candidatePeak='chr1:81700
                 return( pdr)
         }
 
-
-        pdrA = calculate_PDR(groupA, numCellsA, vals_groupA ,candidatePeak, FinalBins_groupA)
-        pdrB = calculate_PDR(groupB, numCellsB, vals_groupB,candidatePeak, FinalBins_groupB)    
-
-
-        res = data.frame(
-            Peak=candidatePeak,
-            ES=wilcox.test(vals_groupA, vals_groupB)$statistic,
-            P.value=wilcox.test(vals_groupA, vals_groupB)$p.value,
-            PDR_A=pdrA,
-            L1A_avg=mean(vals_groupA),
-            PDR_B= pdrB,
-            L1B_avg=mean(vals_groupB)
-            )
-
-        return(res)
         
+        
+        findDifferentialPeak_pdr  <- function(candidatePeak){
+        
+            vals_groupA <- as.numeric(sapply(groupA[['scMACS_peaks']][sample_names_grA],
+                                function(x) x$lambda1[x$PeakID ==candidatePeak]
+                                )
+                                      )
+
+            vals_groupB <- as.numeric(sapply(groupB[['scMACS_peaks']][sample_names_grB],
+                                function(x)  x$lambda1[x$PeakID ==candidatePeak]
+                                ) 
+                                      )
+
+            vals_groupB[is.na(vals_groupB)] <- 0 
+            vals_groupA[is.na(vals_groupA)] <- 0 
+
+        
+            pdrA = calculate_PDR(groupA, numCellsA, vals_groupA ,candidatePeak, FinalBins_groupA)
+            pdrB = calculate_PDR(groupB, numCellsB, vals_groupB,candidatePeak, FinalBins_groupB)    
+
+
+            res = data.frame(
+                Peak=candidatePeak,
+                ES=wilcox.test(vals_groupA, vals_groupB)$statistic,
+                P.value=wilcox.test(vals_groupA, vals_groupB)$p.value,
+                PDR_A=pdrA,
+                L1A_avg=mean(vals_groupA),
+                PDR_B= pdrB,
+                L1B_avg=mean(vals_groupB)
+                )
+
+            return(res)
+        
+        }
+        
+              
+        if(doPDRAnalysis){
+            
+            
+            FinalBins_groupA <- determine_dynamic_range(SimpleList(groupA$SampleFragments),
+                                                 ArchRProj, 
+                                                 binSize=500, 
+                                                 doBin=FALSE)
+
+            FinalBins_groupB <- determine_dynamic_range(SimpleList(groupB$SampleFragments),
+                                                 ArchRProj, 
+                                                 binSize=500, 
+                                                 doBin=FALSE)
+            
+            
+        ## If a sample had > 5 cells but no reads,
+        ## lambda1 is designated as 0. So here 
+        ## we change the NAs (R-artifact) to 0s 
+                
+        diffPeaks <- mclapply(candidatePeaks, 
+                              function(x)
+                                  findDifferentialPeak_pdr(x),
+                              mc.cores = 3
+                              )
+                              
+        diffPeaks <- do.call(rbind, diffPeaks)
+
+        } else { 
+            
+                  findDifferentialPeak  <- function(candidatePeak){
+
+                            vals_groupA <- as.numeric(sapply(groupA[['scMACS_peaks']][sample_names_grA],
+                                                function(x) x$lambda1[x$PeakID ==candidatePeak]
+                                                )
+                                                      )
+
+                            vals_groupB <- as.numeric(sapply(groupB[['scMACS_peaks']][sample_names_grB],
+                                                function(x)  x$lambda1[x$PeakID ==candidatePeak]
+                                                ) 
+                                                      )
+
+                            vals_groupB[is.na(vals_groupB)] <- 0 
+                            vals_groupA[is.na(vals_groupA)] <- 0 
+
+                            chisq_mat <- rbind(c(sum(vals_groupA >0), length(vals_groupA)),
+                                               c(sum(vals_groupB >0), length(vals_groupB)))
+
+                            Mat <- as.table(chisq_mat)
+                            dimnames(Mat) <- list(Group = c("A", "B"),
+                                                  Peak = c("Positive","Total"))
+                      
+                            wilcoxon_p <- wilcox.test(vals_groupA, vals_groupB)
+                            chisq_p <- chisq.test(chisq_mat)
+
+                      
+                            res = data.frame(
+                                Peak=candidatePeak,
+                                ES_wilc=wilcoxon_p$statistic,
+                                Wilcoxon=wilcoxon_p$p.value,
+                                ES_Chisq=chisq_p$statistic,
+                                Chisquare = chisq_p$p.value,
+                                MinPval = min(wilcoxon_p$p.value, chisq_p$p.value),
+                                L1A_avg=round(mean(vals_groupA),4),
+                                L1B_avg=round(mean(vals_groupB),4)
+                                )
+                      
+                            res$FDR =  stats::p.adjust(res$MinPval, method='BH')
+
+                            return(res)
+
+                
+                  }
+
+                
+            diffPeaks <- mclapply(candidatePeaks, 
+                              function(x)
+                                  findDifferentialPeak(x),
+                              mc.cores = numCores
+                              )
+                              
+            diffPeaks <- do.call(rbind, diffPeaks)
+    
+            
+            
+            
+            
+        }
+
+
+
+                              
+                                      
+        return(diffPeaks)                      
         
     }
 
