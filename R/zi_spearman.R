23d66b0dae71cddfcd49558bae928d79f746374a
SamirRachidZaim-AIFI <samir.rachidzaim@alleninstitute.org>
2022-05-19 18:43:53 +0000

    initial commit

---
+++ R/zi_spearman.R
@@ -0,0 +1,90 @@
+#######################################################################################
+#' scHOT implemented Pimentel's zero-inflated (ZI) correlation
+#' in their R package, providing implementations 
+#' of the ZI spearman and tau rank correlations. 
+#' scMACS implements a slight modification of 
+#' scHOT's zero-inflated correlation measure, by
+#' returning NAs in the cases where the correlation
+#' is undefined. Both references are provided below and are
+#' referenced in all documentations to indicate
+#' their work in implementing these methods. 
+#'
+#'
+#' Description: 
+#' the weightedZISpearman function calculates weighted rho\*,
+#' where rho\* is described in Pimentel et al (2009).
+#' This association measure is defined for zero-inflated,
+#' non-negative random variables.
+#'
+#' @title weightedZISpearman
+#' @param w weight vector, values should be between 0 and 1
+#' @param x x and y are non-negative data vectors
+#' @param y x and y are non-negative data vectors
+#' @return \code{numeric} weighted rho* association value between x and y
+#'
+#'
+#' scHOT 
+#' Ghazanfar, S., Lin, Y., Su, X. et al. Investigating higher-order interactions in 
+#'   single-cell data with scHOT. Nat Methods 17, 799–806 (2020). 
+#'   https://doi.org/10.1038/s41592-020-0885-x
+#'
+#' Zero-Inflated Correlation
+#' Pimentel, Ronald Silva, "Kendall's Tau and Spearman's Rho for
+#'   Zero-Inflated Data" (2009). Dissertations. 721.
+#'   https://scholarworks.wmich.edu/dissertations/721
+#'
+#' @export
+
+weightedZISpearman <- function(x, y, w = 1) {
+
+  # needs the original values, not the ranks
+
+  if (any(x < 0 | y < 0)) {
+    stop("x and/or y values have negative values")
+  }
+  if (length(x) != length(y)) {
+    stop("x and y should have the same length")
+  }
+  if (length(w) == 1) {
+    w <- rep(w, length(x))
+  }
+
+  posx = x > 0
+  posy = y > 0
+  pospos = posx & posy
+
+  p_11 = sum(w * pospos)/sum(w)
+  p_00 = sum(w * (!posx & !posy))/sum(w)
+  p_01 = sum(w * (!posx & posy))/sum(w)
+  p_10 = sum(w * (posx & !posy))/sum(w)
+  rho_11 = weightedSpearman(x = x[pospos], y = y[pospos], w = w[pospos])
+  rho_star = p_11 * (p_01 + p_11) * (p_10 + p_11) * rho_11 +
+    3*(p_00 * p_11 - p_10 * p_01)
+
+  if (is.na(rho_star)) {
+    print("Zero inflated Spearman correlation is undefined,
+          returning NA")
+    rho =NA
+    return(rho)
+  }
+
+  return(rho_star)
+}
+
+
+
+weightedSpearman = function(x, y, w = 1) {
+
+  if (length(x) != length(y)) {
+    stop("x and y should have the same length")
+  }
+  if (length(w) == 1) {
+    w <- rep(w, length(x))
+  }
+
+  keep = w > 0
+
+  xr = rank(x[keep])
+  yr = rank(y[keep])
+  return(weightedPearson(x = xr, y = yr, w = w[keep]))
+}
