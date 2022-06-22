90ccdb8fd3d030ae7725f163adff70609ac67873
SamirRachidZaim-AIFI <samir.rachidzaim@alleninstitute.org>
2022-06-22 22:04:48 +0000

    updating bug on cell name loops

--- R/callPeaks_sample.R
+++ R/callPeaks_sample.R
@@ -6,16 +6,16 @@
 #'
 #'
 #' @param ArchRProj an ArchR Project 
-#' @param cellSubsets vector of strings. Cell subsets for which to call peaks. Optional, if cellSubsets='ALL', then peak calling is done on all cell populations in the ArchR project metadata
-#' @param cellCol_label_name string indicating which column in the meta data file contains 
+#' @param metadf meta data file
+#' @param cellType_Samples vector of strings. CellType samples to call peaks on.
+#' @param metaColumn string indicating which column in the meta data file contains 
 #'        the cell population label
-#' @param sample_label_name string indicating which column in the meta data file contains 
-#'        the samples
 #'
 #' @param returnAllPeaks boolean. Indicates whether scMACS should return object containing all genomic regions or just the positive (+) called peaks. Default to the latter, only positive peaks. 
 #'
 #' @param numCores integer. Number of cores to parallelize peak-calling across
 #'                 multiple cell populations
+#' @param returnFrags boolean. Determines if fragment files should be returned
 #'
 #' @return scMACs_PeakList an list containing peak calls for each cell population passed on in the 
 #'         cell subsets argument. Each peak call is returned as as Genomic Ranges object.
@@ -28,27 +28,34 @@
 
 callPeaks_by_sample <- function(ArchRProj, 
                                 metadf,
-                                cellType_to_analyze='CD14 Mono',
-                                cellType_sample_label_name='cellType_sample',         
+                                cellType_Samples,
+                                metaColumn,
                                 returnAllPeaks=TRUE,
-                                numCores=30
+                                numCores=30,
+                                returnFrags=F
                      
                      ){
     
     ## identify cell population to export 
     ## across samples by cell types
     
-    cellTypesToExport <- unique(cellTypesToExport)
+    cellType_Samples <- unique(cellType_Samples)
     frags <- getPopFrags(ArchRProj, 
-                         metaColumn = cellType_sample_label_name, 
-                         cellSubsets = cellTypesToExport, 
+                         metaColumn = metaColumn, 
+                         cellSubsets = cellType_Samples, 
                          numCores= numCores)
-
-    names(frags) <- cellTypesToExport
-
+    
+    pryr::mem_used()
+    sample_names <- names(frags)
     ## identify cell barcodes 
-    normalization_factors <- sapply(frags, length)
-
+    normalization_factors <- as.numeric(sapply(frags, length))
+    
+    
+    ## add preFactor multiplier across datasets
+    curr_frags_median <- median(ArchRProj@cellColData$nFrags)
+    study_prefactor = 3668/curr_frags_median # training median
+    
+    normalization_factors = normalization_factors 
     # ###########################################################
     # ###########################################################
     # ## call scMACS peaks 
@@ -59,35 +66,34 @@ callPeaks_by_sample <- function(ArchRProj,
 
     barcodes <- lapply(frags, function (x) unique(x$RG)
                )
-
-    subset_ArchR_projects <- mclapply(barcodes, function(x) 
-           subsetCells(covidArchR, x),
-                                      mc.cores=numCores
+    
+    subset_ArchR_projects <- lapply(barcodes, function(x) 
+           subsetCells(ArchRProj, x)
            )
 
     pryr::mem_used()
-
+                               
     ### call peaks using 
     ### scMACS functionalities
     peak_lists <- mclapply(1:length(subset_ArchR_projects),
                            function(x)
 
-                           try(callPeaks_by_population(ArchRProj=subset_ArchR_projects[[x]],
-                                            cellSubsets=cellTypesToExport[x],
-                                            cellCol_label_name=cellType_sample_label_name,
+                    callPeaks_by_population(ArchRProj=subset_ArchR_projects[[x]],
+                                            cellSubsets=sample_names[x],
+                                            cellCol_label_name=metaColumn,
                                             returnAllPeaks=TRUE,
                                             numCores=1,
                                             totalFrags=normalization_factors[x],
-                                            fragsList =frags[[x]]
-                           )),
+                                            fragsList =frags[[x]],
+                                            StudypreFactor = study_prefactor
+                           ),
                           mc.cores=numCores
 
                         )
     
-    finalObject = list(Sample_peaks=peak_lists,
-                       Sample_fragments=frags)
+    rm(subset_ArchR_projects, frags)
+    pryr::mem_used()
     
-    return(finalObject)
-    
+    return(peak_lists)
 
 }
