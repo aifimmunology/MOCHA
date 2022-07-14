3af37160742afadeddc2243e9cbcd7adfc733bf0
Imran McGrath <imran.mcgrath@alleninstitute.org>
2022-07-14 08:47:13 +0000

    Basic new reproduciblePeaks and samplePeakMatrix functionality

--- R/create_sample_peak_matrix.R
+++ R/create_sample_peak_matrix.R
@@ -6,9 +6,9 @@
 #'              each sample.
 #'
 #'
-#' @param sample_specific_peaks output of calls_peaks_by_sample (peaks only)
-#' @param union_peaks: a vector containing the tileIDs to subset the peak matrix
-#' @return sample_peak_matrix a sample X peak matrix containing observed 
+#' @param peakCallResults output of calls_peaks_by_sample
+#' @param reproduciblePeaks a vector containing the tileIDs to subset the peak matrix
+#' @return samplePeakIntensityMat a sample X peak matrix containing observed 
 #'         measurements for each sample at each peak. 
 #' 
 #' @details The technical details of the algorithm are found in XX.
@@ -17,48 +17,66 @@
 #'
 #' @export
 
-create_peak_sampleMatrix <- function(sample_specific_peaks, union_peaks){
+create_peak_sampleMatrix <- function(
+    peakCallResults,
+    cellPopulation,
+    reproduciblePeaks
+){
     
+    # Get the RaggedExperiment for this cellPopulation
+    peaksExperiment <- peakCallResults[[cellPopulation]]
+    
+    # Extract matrices of samples by peak tileIDs with TotalIntensity
+    samplePeakIntensityMat <- RaggedExperiment::compactAssay(
+        peaksExperiment, i="TotalIntensity")
+    
+    # Replace NAs with zeroes for zero-inflated hypothesis testing 
+    samplePeakIntensityMat[is.na(samplePeakIntensityMat)] <- 0
+    
+    # Filter to just peaks in the given reproduciblePeaks
+    samplePeakIntensityMat <- samplePeakIntensityMat[reproduciblePeaks, ]
+    
+    return(samplePeakIntensityMat)
         
-    ## Extract Peak list from Sample Specific Peaks Object 
-    sample_names <-  sapply(sample_specific_peaks, function(x) names(x))
+#     ## Extract Peak list from Sample Specific Peaks Object 
+#     sample_names <-  sapply(sample_specific_peaks, function(x) names(x))
     
-    ## assign sample names to intensity matrix
-    for(i in 1:length(sample_specific_peaks)){
+#     ## assign sample names to intensity matrix
+#     for(i in 1:length(sample_specific_peaks)){
 
-        sample_specific_peaks[[i]][[1]]$Sample <- sample_names[i]
+#         sample_specific_peaks[[i]][[1]]$Sample <- sample_names[i]
 
-    }
+#     }
     
-    ## extract matrices
-    sample_specific_peaks <- lapply(sample_specific_peaks,
-                        function(x) x[[1]]
-                        )
+#     ## extract matrices
+#     sample_specific_peaks <- lapply(sample_specific_peaks,
+#                         function(x) x[[1]]
+#                         )
     
-    ## concatenate matrix 
-    fullMatrix <- rbindlist(sample_specific_peaks)
+#     ## concatenate matrix 
+#     fullMatrix <- rbindlist(sample_specific_peaks)
     
-    ## Filter and rename feature names
-    fullMatrix <-  fullMatrix[,c('TotalIntensity','tileID','Sample')]
-    colnames(fullMatrix)[1] <- 'lambda1'
+#     ## Filter and rename feature names
+#     fullMatrix <-  fullMatrix[,c('TotalIntensity','tileID','Sample')]
+#     colnames(fullMatrix)[1] <- 'lambda1'
 
-    ## Reshape long to wide to create 
-    ## Sample X Peak matrix. 
-    sample_peak_matrix <- data.table::dcast(fullMatrix, 
-                                            formula=tileID ~ Sample, 
-                                            value.var='lambda1')
+#     ## Reshape long to wide to create 
+#     ## Sample X Peak matrix. 
+#     sample_peak_matrix <- data.table::dcast(fullMatrix, 
+#                                             formula=tileID ~ Sample, 
+#                                             value.var='lambda1')
     
-    ## Replace NAs with zeroes for zero-inflated
-    ## hypothesis testing 
-    sample_peak_matrix[is.na(sample_peak_matrix)] <- 0 
+#     ## Replace NAs with zeroes for zero-inflated
+#     ## hypothesis testing 
+#     sample_peak_matrix[is.na(sample_peak_matrix)] <- 0 
     
-    if(is.null(union_peaks)){
+#     if(is.null(union_peaks)){
         
-        return(sample_peak_matrix)
+#         return(sample_peak_matrix)
         
-    } else { 
+#     } else { 
         
-        return(sample_peak_matrix[tileID %in% union_peaks])
-    }
+#         return(sample_peak_matrix[tileID %in% union_peaks])
+#     }
         
 }
--- R/get_reproducible_peaks.R
+++ R/get_reproducible_peaks.R
@@ -1,55 +1,53 @@
 #' @title \code{get_reproducible_peaks}
 #'
 #' @description \code{get_reproducible_peaks} is an R helper function, part of the single-cell peak 
-#'
+#' 
+#' @export
 
-get_reproducible_peaks <- function(sample_specific_peaks, metaFile, 
-                                   reproducibility=0.2, 
-                                   fname='cd16_peaks.png'){
+get_reproducible_peaks <- function(
+    peakCallResults,
+    cellPopulation,       
+    reproducibility = 0.2
+){
+
+    # Get the RaggedExperiment for this cellPopulation
+    peaksExperiment <- peakCallResults[[cellPopulation]]
+    nSamples <- length(colnames(peaksExperiment))
     
+    # Extract matrices of samples by peak tileIDs with peak status
+    # and set NA to FALSE (no peak here)
+    samplePeakMat <- RaggedExperiment::compactAssay(
+        peaksExperiment, i="peak")
+    samplePeakMat[is.na(samplePeakMat)] <- FALSE
     
-    ## Extract Peak list from Sample Specific Peaks Object 
-    sample_names <-  sapply(sample_specific_peaks, function(x) names(x))
-
-
-    ## Reformat peak information to 
-    ## obtain the # of peaks reproducible
-    ## across 0-50% of the samples
-    lambda1 <- lapply(1:length(sample_specific_peaks),
-                      function(x) 
-                          data.table(Sample=sample_names[x],
-                                     Intensity=sample_specific_peaks[[x]][[1]]$TotalIntensity,
-                                     tileID=sample_specific_peaks[[x]][[1]]$tileID,
-                                     Peak=sample_specific_peaks[[x]][[1]]$peak,
-                                     Index=ifelse(metaFile$Class[x]=='Positive',1,0)
-                                    )
-                      )
-    lambda_mat <- rbindlist(lambda1)
-
-    #######################################################################
-    #######################################################################
-    # Plot sample-specific reproducible peaks
-
-    counts_by_group <- plot_sample_specific_reproducible_peaks(lambda_mat,
-                                                                 save_plot=TRUE,
-                                                                 fname=fname)
+    # Count the number of TRUE peaks in each row and divide by nSamples
+    percentTruePeaks <- rowSums(samplePeakMat) / nSamples 
     
-    ### plot Venn diagram 
-    Repro_Limit = reproducibility
-    reproducible_casePeaks = counts_by_group$CasePeaks[V1>= Repro_Limit]
-    reproducible_controlPeaks = counts_by_group$ControlPeaks[V1>= Repro_Limit]
-
-    # Generate the two sets for 
-    # the venn diagramß
-    set1 <- reproducible_casePeaks$tileID
-    set2 <- reproducible_controlPeaks$tileID
-    common_peaks <- intersect(set1,set2)
-    union_peaks <- union(set1, set2)
+    # Keep only peaks with reproducibility above specified threshold
+    reproduciblePeaks <- percentTruePeaks[percentTruePeaks >= reproducibility]
+    reproduciblePeaks <- names(reproduciblePeaks)
     
-    results <- list(CasePeaks=set1,
-                    ControlPeaks=set2,
-                    Intersection=common_peaks,
-                    Union=union_peaks)
+    return(reproduciblePeaks)
+    
+    ##################################
+    ##################################
+    ##################################
+
+#     reproducible_casePeaks = counts_by_group$CasePeaks[V1>= Repro_Limit]
+#     reproducible_controlPeaks = counts_by_group$ControlPeaks[V1>= Repro_Limit]
+
+#     # Generate the two sets for 
+#     # the venn diagramß
+#     set1 <- reproducible_casePeaks$tileID
+#     set2 <- reproducible_controlPeaks$tileID
+#     common_peaks <- intersect(set1,set2)
+#     union_peaks <- union(set1, set2)
+    
+#     results <- list(CasePeaks=set1,
+#                     ControlPeaks=set2,
+#                     Intersection=common_peaks,
+#                     Union=union_peaks)
                     
-    return(results)
+#     return(results)
+    
 }
