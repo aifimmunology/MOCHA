b7d365fb3b8848d5e371aea6811c34f2cb0edc64
Imran McGrath <imran.mcgrath@alleninstitute.org>
2022-08-02 00:53:02 +0000

    DOC update readme

--- vignettes/COVID_example.R
+++ vignettes/COVID_example.R
@@ -3,12 +3,13 @@ library(ArchR)
 library(devtools)
 library(scMACS)
 
-
 # Load the ArchR Project
-# You should substitute this with your own ArchR project
-covidArchR <- ArchR::loadArchRProject("/home/jupyter/FullCovid")
+# You should substitute this with your own ArchR project.
+# You must have completed cell labeling with your ArchR project.
+myArchRProj <- ArchR::loadArchRProject("/home/jupyter/FullCovid")
 
 # For our example: filter ArchR Project to three samples from each Covid Status
+# This is not strictly necessary for your ArchR Project 
 samplesToKeep <- c(
   "B011-AP0C1W3", 
   "B011-AP0C1W8", 
@@ -17,23 +18,26 @@ samplesToKeep <- c(
   "B025_FSQAAZ0C0YJ-01",
   "B025_FSQAAZ0C00P-07"
 )
-idxSample <- BiocGenerics::which(covidArchR$Sample %in% samplesToKeep)
-cellsSample <- covidArchR$cellNames[idxSample]
-covidArchR <- covidArchR[cellsSample, ]
+idxSample <- BiocGenerics::which(myArchRProj$Sample %in% samplesToKeep)
+cellsSample <- myArchRProj$cellNames[idxSample]
+myArchRProj <- myArchRProj[cellsSample, ]
 
 ####################################################
 # 1. Setting Parameters
+#    These should be set according to your ArchR project
+#    and investgative question.
+#
 #    For more details on each of these parameters, 
 #    view the help pages for each function using 
 #    ?callOpenTiles and ?getSampleTileMatrix
 ####################################################
 
 # Parameters for calling open tiles
-cellPopLabel <- "CellSubsets"
+cellPopLabel <- "CellSubsets" 
 cellPopulations <- c("MAIT", "CD16 Mono", "DC")
 numCores <- 20
 
-# Parameters for downstream analysis
+# Parameters for generating the sample-tile matrices
 threshold <- 0.2
 groupColumn <- "COVID_status"
 join <- "union"
@@ -45,12 +49,11 @@ join <- "union"
 ####################################################
 
 tileResults <- scMACS::callOpenTiles( 
-    covidArchR,
+    myArchRProj,
     cellPopLabel = cellPopLabel,
     cellPopulations = cellPopulations,
     numCores = numCores
 )
-
 
 ####################################################
 # 3. Get consensus sample-tile matrices
