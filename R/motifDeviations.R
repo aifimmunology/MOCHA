b0dc69cb3e8feaf4501eadf7983a49431b124e30
Mark-Phillip Pebworth <markphillip.pebworth@alleninstitute.org>
2022-08-02 14:03:37 -0700

    Edits to getMotifDeviations
    Edits based on experience - how to improve motif deviations

--- R/motifDeviations.R
+++ R/motifDeviations.R
@@ -25,10 +25,80 @@ getMotifCoverages <- function(covFiles, MotifLocations, numCores=1){
  return(counts)
 }
 
-    
+
+######## matchPeaks: Function that takes in Sample-Tile matrix and a foreground set of peaks
+## and finds peakset that matches the intensity within the rest of the peaks (or a predefined background set). 
+## @covFiles: GRangesList of coverage for each sample
+## @MotifLocations: GRangesList of Motif locations
+## @numCores: number of cores to parallelize over. 
 
 
+matchPeaks <- function(SampleTileMatrix, ForegroundPeakset, BackgroundSet = NULL, method = 'Mean', N = 50, numCores = 1){
 
+	if(class(ForegroundPeakset)[1] == "Character"){
+
+		ForegroundPeakSet = StringsToGRanges(ForegroundPeakset)
+
+	}else if(class(ForegroundPeakset)[1] != "GRanges"){
+
+		stop('Error: Foreground Peakset must either be a GRanges object or a vector of string in the format Chr1:100-200')
+
+	}
+
+	if(is.null(BackgroundSet)){
+
+		BackgroundSet = plyranges::filter_by_non_overlaps(StringsToGRanges(SampleTileMatrix$tileID), ForegroundPeakset)
+
+	}else if(!is.null(BackgroundSet) & class(BackgroundSet)[1] == "Character"){
+
+		BackgroundSet = StringsToGranges(BackgrounfSet)
+
+	}else if(!is.null(BackgroundSet) & class(BackgroundSet)[1] != "GRanges"){
+
+		stop('Error: BackgroundSet must either be a GRanges object, a vector of string in the format Chr1:100-200, or set to NULL')
+
+	}
+
+	Count1 <- findOverlaps(StringsToGRanges(SampleTileMatrix$tileID),ForegroundPeakset)
+	Count2 <- findOverlaps(StringsToGRanges(SampleTileMatrix$tileID),BackgroundSet)
+
+	#Check if the background and foreground set overlap with any tiles in common. 
+	if(any(queryHits(Count1) %in% queryHits(Count2))){stop('Error: Background & Foreground set overlap for Sample-Tile matrix')}
+
+	ForeMat <- SampleTileMatrix[queryHits(Count1), -'tileID']    
+	
+	BackMat <- SampleTileMatrix[queryHits(Count2), -'tileID'] 	
+
+	if(toLower(Method) == 'mean'){
+
+		avgForeMat <-  rowMeans(as.matrix(ForeMat))
+		avgBackMat <- rowMeans(as.matrix(BackMat)) 
+
+	}else if(toLower(Method) == 'median'){
+
+		avgForeMat <-  rowMedians(as.matrix(ForeMat))
+		avgBackMat <- rowMedians(as.matrix(BackMat)) 
+
+	}else if(toLower(Method) == 'sd'){
+
+		avgForeMat <-  rowSds(as.matrix(ForeMat))
+		avgBackMat <- rowMSds(as.matrix(BackMat)) 
+
+	}else {stop('Error: Method not recognize')}
+	
+                                
+	matchedPeak <- mclapply(avgForeMat, function(x) {
+      		 order(abs(x - avgBackMat))[c(1:N)]
+    	}, mc.cores = numCores)
+
+	if(returnGR){
+		matchedPeakReg = BackgroundSet[sort(unlist(unique(matchedPeak)))]
+		return(matchedPeakReg)
+	}else{
+		subMat <- BackMat[sort(unlist(unique(matchedPeak))),]
+		return(subMat)
+	}
+}
 
 
 
@@ -94,12 +164,62 @@ normalized_wilcoxon <- function(data, group, point.mass=0, test='wilcoxon'){
     }
     return(T2)
 }
-Example
-a = sapply(1:100000, function(x) 
-    { data = rpois(n=100, lambda=20)
-    group = c(rep(1,50),rep(0,50))
-    normalized_wilcoxon(data,group,test='wilcoxon')
-     })
-png('tmp.png')
-hist(a, breaks=100)
-dev.off()
+
+
+
+##### getMotifDev
+
+getMotifDev <- function(MotifCoverage, ForegroundPeaks, BackgroundPeaks,
+				 numCores = 1, testType = 'Wilcoxon', verbose = FALSE){
+
+	ForeSubset <- mclapply(MotifCoverage, function(x) plyranges::filter_by_overlaps(x, ForegroundPeaks), mc.cores = numCores)
+	BackSubset <- mclapply(MotifCoverage, function(x) plyranges::filter_by_overlaps(x, BackgroundPeaks), mc.cores =  numCores)
+	
+	
+	ForeDF <- mclapply(seq_along(MotifCoverage), function(x){
+    
+            tmp1 <- data.frame(GRangesToString(ForeDF[[x]]), ForeDF[[x]]$name, ForeDF[[x]]$score)
+            colnames(tmp1) <-  c('Peak', 'Motif', names(ForeSubset)[x])
+            tmp1
+    	}, mc.cores = 25) %>% purrr::reduce(dplyr::full_join, by= c('Peak', 'Motif'))
+       
+	ForeDF[is.na(ResDFList)]=0
+
+	ResDF <- mclapply(seq_along(MotifCoverage), function(x){
+    
+            tmp1 <- data.frame(GRangesToString(BackSubset[[x]]), BackSubset[[x]]$name, BackSubset[[x]]$score)
+            colnames(tmp1) <-  c('Peak', 'Motif',  names(BackSubset)[x])
+            tmp1
+    	}, mc.cores = 25)  %>% purrr::reduce(dplyr::full_join, by = c('Peak', 'Motif'))    
+	
+
+	ResDF[is.na(ResDF)]=0
+
+	motifList <- names(MotifLocations)
+
+	motifDev <- mclapply(seq_along(motifList), function(x){
+    
+                timeSub <- TimeDFList %>% dplyr::filter(Motif == motifList[x])
+                resSub <- ResDFList %>%  dplyr::filter(Motif == motifList[x])
+                ## Iterate over all samples, comparing each sample to the average. 
+                motif1Dev <- lapply(seq_along(dim(timeSub)[2] -2), function(y){
+                    
+                    comb -> rbind(timeSub[,y+2], resSub[,y+2])
+                    group1 <- c(rep(1, dim(timeSub)[1]), rep(0, dim(resSub)[1]))
+                    
+                    normalized_wilcoxon(comb, group1, test = testType)
+                    
+                }) %>% cbind()
+    
+                colnames(motif1Dev) <- colnames(timeSub)[,-c(1:2)]
+                
+                motif1Dev
+                
+    
+    }, mc.cores = numCores) %>% do.call('rbind', .)
+                                               
+    rownames(motifDev) = motifList
+
+    return(motifDev)
+}
+
