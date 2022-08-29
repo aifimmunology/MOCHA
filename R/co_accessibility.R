a318fc105834258e662e435458ab7d3f02640367
imran-aifi <imran.mcgrath@alleninstitute.org>
2022-08-29 16:39:26 -0700

    STY style with styler, add new dependencies to DESCRIPTION

--- R/co_accessibility.R
+++ R/co_accessibility.R
@@ -7,200 +7,183 @@
 #' @param mat: sample-peak matrix with regions to analyze
 #' @param numCores: integer to determine # of paralell cores
 #'
-#' @return a 3-column data.frame containing 
+#' @return a 3-column data.frame containing
 #'         - Correlation= Zero-inflated Spearman Correlation
 #'         - Peak1= location of co-accessible region 1
 #'         - Peak2= location of co-accessible region 2
-#' 
-#' @details The technical details of the zero-inflated correlation can be
-#'          found here: 
 #'
-#'               Pimentel, Ronald Silva, "Kendall's Tau and Spearman's Rho 
-#'               for Zero-Inflated Data" (2009). Dissertations. 
+#' @details The technical details of the zero-inflated correlation can be
+#'          found here:
+#'
+#'               Pimentel, Ronald Silva, "Kendall's Tau and Spearman's Rho
+#'               for Zero-Inflated Data" (2009). Dissertations.
 #'
 #'          while the implementation (scHOT R package), can be found here:
 #'               http://www.bioconductor.org/packages/release/bioc/html/scHOT.html
 #'
 #' @references XX
-#' @examples 
-#' Generate 
-#' mat1 = matrix(pmax(0, rnorm(1000)), ncol=100); row.names(mat1) <- paste('A',1:10,sep='_')
-#' ziSpear_mat <- co_accessibility(mat1, numCores=5)
+#' @examples
+#' Generate
+#' mat1 <- matrix(pmax(0, rnorm(1000)), ncol = 100)
+#' row.names(mat1) <- paste("A", 1:10, sep = "_")
+#' ziSpear_mat <- co_accessibility(mat1, numCores = 5)
 #' head(ziSpear_mat)
 #' @export
 
-co_accessibility <- function(subMat, filterPairs = NULL, index = NULL, numCores=40, verbose = FALSE){
-  
-  ## Prep data.table of peaks for correlations. 
-  ##mat1 <- as.data.table(mcols(GR))
-  ##rownames(mat1) <- GRangesToString(GR)
-  
-  #mat1 <- subMat[,-c('tileID','chr', 'start', 'end')]
-  #rownames(mat1) = subMat$tileID
-  pairwise_combos = RcppAlgos::comboGrid(rownames(subMat),rownames(subMat), repetition = FALSE)
+co_accessibility <- function(subMat, filterPairs = NULL, index = NULL, numCores = 40, verbose = FALSE) {
 
-  ##Pull out only combinations that involve the peak of interest
-  if(!is.null(index) & class(index) =='integer'){
-    
-    pairwise_combos <- pairwise_combos[pairwise_combos[,'Var1'] %in% rownames(subMat)[index] | 
-                                         pairwise_combos[,'Var2'] %in% rownames(subMat)[index] ,]
-    
-  }else if(verbose){ print('No valid index given. Testing all peaks within range.')}
-  
-  
+  ## Prep data.table of peaks for correlations.
+  ## mat1 <- as.data.table(mcols(GR))
+  ## rownames(mat1) <- GRangesToString(GR)
+
+  # mat1 <- subMat[,-c('tileID','chr', 'start', 'end')]
+  # rownames(mat1) = subMat$tileID
+  pairwise_combos <- RcppAlgos::comboGrid(rownames(subMat), rownames(subMat), repetition = FALSE)
+
+  ## Pull out only combinations that involve the peak of interest
+  if (!is.null(index) & class(index) == "integer") {
+    pairwise_combos <- pairwise_combos[pairwise_combos[, "Var1"] %in% rownames(subMat)[index] |
+      pairwise_combos[, "Var2"] %in% rownames(subMat)[index], ]
+  } else if (verbose) {
+    print("No valid index given. Testing all peaks within range.")
+  }
+
+
   ## Filter out any pairs that have already been tested, and see if anything remains.
-  if(!is.null(filterPairs) & length(pairwise_combos) > 2){
-    
-    #If there's more than one pair of peaks, filter out all the previously tested ones. 
-    pairwise_combos <- pairwise_combos[!(pairwise_combos[,'Var1'] %in% filterPairs$Peak1 &
-                                           pairwise_combos[,'Var2'] %in% filterPairs$Peak2),]
-    
-  }else if(!is.null(filterPairs) & length(pairwise_combos) > 0){
-    
-    #If only one pair of peaks is left, and that pair has already been tested, then return NULL
-    if(pairwise_combos[1] %in% filterPairs$Peak1 & pairwise_combos[2] %in% filterPairs$Peak2){
+  if (!is.null(filterPairs) & length(pairwise_combos) > 2) {
+
+    # If there's more than one pair of peaks, filter out all the previously tested ones.
+    pairwise_combos <- pairwise_combos[!(pairwise_combos[, "Var1"] %in% filterPairs$Peak1 &
+      pairwise_combos[, "Var2"] %in% filterPairs$Peak2), ]
+  } else if (!is.null(filterPairs) & length(pairwise_combos) > 0) {
+
+    # If only one pair of peaks is left, and that pair has already been tested, then return NULL
+    if (pairwise_combos[1] %in% filterPairs$Peak1 & pairwise_combos[2] %in% filterPairs$Peak2) {
       return(NULL)
     }
-
   }
-  
-  
-  ### Loop through all pairwise 
-  ### combinations of peaks 
-  
-  
-  ## if only one pair of peaks left, then nrows will hit an error. length works whether there is one pair or many left.  
-  #If no peaks 
-  if(length(pairwise_combos) == 0 & verbose){
-    
-    print('Warning: No peaks found in neighborhood of region of interest')
+
+
+  ### Loop through all pairwise
+  ### combinations of peaks
+
+
+  ## if only one pair of peaks left, then nrows will hit an error. length works whether there is one pair or many left.
+  # If no peaks
+  if (length(pairwise_combos) == 0 & verbose) {
+    print("Warning: No peaks found in neighborhood of region of interest")
     return(NULL)
-  
-  }else if(length(pairwise_combos) == 0) { 
-  
-      return(NULL)
-    
-  }else if(length(pairwise_combos) == 2) { 
-    
-    #If only one pair of peaks to test, then it's no longer a data.frame, but a vector.
-    zero_inflated_spearman <- weightedZISpearman(x= subMat[pairwise_combos[1],],
-                                                 y= subMat[pairwise_combos[2],])
-    
-    zi_spear_mat <- data.table(Correlation=zero_inflated_spearman,
-                               Peak1= pairwise_combos[1],
-                               Peak2= pairwise_combos[2])
-    
-    
-  }else{
-  
-    zero_inflated_spearman <- unlist(mclapply(1:nrow(pairwise_combos),
-                                            function(x)
-                                              weightedZISpearman(x= subMat[pairwise_combos[x,'Var1'],],
-                                                                 y= subMat[pairwise_combos[x,'Var2'],]
-                                              ),
-                                            mc.cores=numCores
-                                    ))
-    
-    
-    #return(zero_inflated_spearman)
-    ### Create zero-inflated correlation matrix
-    ### from correlation values, 
-    zi_spear_mat <- data.table(Correlation=zero_inflated_spearman,
-                               Peak1= pairwise_combos[,'Var1'],
-                               Peak2= pairwise_combos[,'Var2']
+  } else if (length(pairwise_combos) == 0) {
+    return(NULL)
+  } else if (length(pairwise_combos) == 2) {
+
+    # If only one pair of peaks to test, then it's no longer a data.frame, but a vector.
+    zero_inflated_spearman <- weightedZISpearman(
+      x = subMat[pairwise_combos[1], ],
+      y = subMat[pairwise_combos[2], ]
     )
-    
+
+    zi_spear_mat <- data.table(
+      Correlation = zero_inflated_spearman,
+      Peak1 = pairwise_combos[1],
+      Peak2 = pairwise_combos[2]
+    )
+  } else {
+    zero_inflated_spearman <- unlist(mclapply(1:nrow(pairwise_combos),
+      function(x) {
+        weightedZISpearman(
+          x = subMat[pairwise_combos[x, "Var1"], ],
+          y = subMat[pairwise_combos[x, "Var2"], ]
+        )
+      },
+      mc.cores = numCores
+    ))
+
+
+    # return(zero_inflated_spearman)
+    ### Create zero-inflated correlation matrix
+    ### from correlation values,
+    zi_spear_mat <- data.table(
+      Correlation = zero_inflated_spearman,
+      Peak1 = pairwise_combos[, "Var1"],
+      Peak2 = pairwise_combos[, "Var2"]
+    )
   }
-  
+
   return(zi_spear_mat)
-  
-}         
+}
 
 
 
 
-FindCoAccessibleLinks <- function(peakDT,regions, windowSize = 1*10^6, numCores = 1,  verbose = FALSE){
-  
-  peakDF <- as.data.frame(peakDT[,-'tileID'])
+FindCoAccessibleLinks <- function(peakDT, regions, windowSize = 1 * 10^6, numCores = 1, verbose = FALSE) {
+  peakDF <- as.data.frame(peakDT[, -"tileID"])
   rownames(peakDF) <- peakDT$tileID
-  
-  start <- as.numeric(gsub('chr.*\\:|\\-.*','',peakDT$tileID))
-  end <- as.numeric(gsub('chr.*\\:|.*\\-','',peakDT$tileID))
-  chr <- gsub('\\:.*','',peakDT$tileID)
-  
+
+  start <- as.numeric(gsub("chr.*\\:|\\-.*", "", peakDT$tileID))
+  end <- as.numeric(gsub("chr.*\\:|.*\\-", "", peakDT$tileID))
+  chr <- gsub("\\:.*", "", peakDT$tileID)
+
   regionDF <- as.data.frame(regions)
- 
-  #Initialize Correlation Datatable. 
+
+  # Initialize Correlation Datatable.
   PeakCorr <- NULL
-  pb = txtProgressBar(min = 0, max = length(regions), initial = 0)  
-  
-  for(i in 1:length(regions)){
-      #print(i)
-      setTxtProgressBar(pb,i)
-      
-      ## Find all peaks in the next window
-      window_tmp <- which(start > regionDF$start[i] - windowSize/2 &
-                           end < regionDF$end[i] + windowSize/2 & 
-                           chr == regionDF$seqnames[i])
-      
-      if(length(window_tmp) > 1){
-        
-        #The region of interest should overlap with the peak at least partially.
-        keyPeak <- which(
-                          window_tmp == which(
-                            ((start <= regionDF$start[i] & end >= regionDF$start[i]) |
-                             (start >= regionDF$start[i] & end<= regionDF$end[i])  |
-                              (start <= regionDF$end[i] & end >= regionDF$end[i])) & 
-                           chr %in% regionDF$seqnames[i]))
-        
-        nextCorr <- co_accessibility(peakDF[window_tmp,],  filterPairs = PeakCorr, numCores=numCores,
-                                     index = keyPeak, verbose = verbose)
-        PeakCorr <- rbind(PeakCorr, nextCorr)
-        
-      }else if(verbose){
-        print('Warning: No correlations found for given region.')
-      }
-      
+  pb <- txtProgressBar(min = 0, max = length(regions), initial = 0)
+
+  for (i in 1:length(regions)) {
+    # print(i)
+    setTxtProgressBar(pb, i)
+
+    ## Find all peaks in the next window
+    window_tmp <- which(start > regionDF$start[i] - windowSize / 2 &
+      end < regionDF$end[i] + windowSize / 2 &
+      chr == regionDF$seqnames[i])
+
+    if (length(window_tmp) > 1) {
+
+      # The region of interest should overlap with the peak at least partially.
+      keyPeak <- which(
+        window_tmp == which(
+          ((start <= regionDF$start[i] & end >= regionDF$start[i]) |
+            (start >= regionDF$start[i] & end <= regionDF$end[i]) |
+            (start <= regionDF$end[i] & end >= regionDF$end[i])) &
+            chr %in% regionDF$seqnames[i]
+        )
+      )
+
+      nextCorr <- co_accessibility(peakDF[window_tmp, ],
+        filterPairs = PeakCorr, numCores = numCores,
+        index = keyPeak, verbose = verbose
+      )
+      PeakCorr <- rbind(PeakCorr, nextCorr)
+    } else if (verbose) {
+      print("Warning: No correlations found for given region.")
+    }
   }
 
   close(pb)
   gc()
   return(PeakCorr)
-  
-}  
+}
 
-FilterCoAccessibleLinks <- function(PeakCorr, threshold = 0.5){
-  
-  if(!any(abs(PeakCorr$Correlation) > threshold)){
-    stop('Error: There are no values above the threshold.')  
-  
+FilterCoAccessibleLinks <- function(PeakCorr, threshold = 0.5) {
+  if (!any(abs(PeakCorr$Correlation) > threshold)) {
+    stop("Error: There are no values above the threshold.")
   }
-  
-  PeakCorr1 <- PeakCorr[abs(PeakCorr$Correlation) > threshold,]
-  start1 <- as.numeric(gsub('chr.*\\:|\\-.*','',  PeakCorr1$Peak1))
-  end1 <- as.numeric(gsub('chr.*\\:|.*\\-','', PeakCorr1$Peak1))
-  
-  start2 <- as.numeric(gsub('chr.*\\:|\\-.*','', PeakCorr1$Peak2))
-  end2 <- as.numeric(gsub('chr.*\\:|.*\\-','', PeakCorr1$Peak2))
-  
-  PeakCorr1$chr <- gsub('\\:.*','',PeakCorr1$Peak2)
-  PeakCorr1$start <-  apply(data.table(start1, start2), 1, min)
-  PeakCorr1$end <-  apply(data.table(end1, end2), 1, max)
+
+  PeakCorr1 <- PeakCorr[abs(PeakCorr$Correlation) > threshold, ]
+  start1 <- as.numeric(gsub("chr.*\\:|\\-.*", "", PeakCorr1$Peak1))
+  end1 <- as.numeric(gsub("chr.*\\:|.*\\-", "", PeakCorr1$Peak1))
+
+  start2 <- as.numeric(gsub("chr.*\\:|\\-.*", "", PeakCorr1$Peak2))
+  end2 <- as.numeric(gsub("chr.*\\:|.*\\-", "", PeakCorr1$Peak2))
+
+  PeakCorr1$chr <- gsub("\\:.*", "", PeakCorr1$Peak2)
+  PeakCorr1$start <- apply(data.table(start1, start2), 1, min)
+  PeakCorr1$end <- apply(data.table(end1, end2), 1, max)
 
   return(PeakCorr1)
 }
 
 
 #############################################
-
-
-
-
-
-
-
-
-
-
-
-
--- R/zi_spearman.R
+++ R/zi_spearman.R
@@ -1,17 +1,17 @@
 #######################################################################################
 #' scHOT implemented Pimentel's zero-inflated (ZI) correlation
-#' in their R package, providing implementations 
-#' of the ZI spearman and tau rank correlations. 
-#' scMACS implements a slight modification of 
+#' in their R package, providing implementations
+#' of the ZI spearman and tau rank correlations.
+#' scMACS implements a slight modification of
 #' scHOT's zero-inflated correlation measure, by
 #' returning NAs in the cases where the correlation
-#' is undefined, and modifying it to use a C-backed correlation program. 
+#' is undefined, and modifying it to use a C-backed correlation program.
 #' Both references are provided below and are
 #' referenced in all documentations to indicate
-#' their work in implementing these methods. 
+#' their work in implementing these methods.
 #'
 #'
-#' Description: 
+#' Description:
 #' the weightedZISpearman function calculates weighted rho\*,
 #' where rho\* is described in Pimentel et al (2009).
 #' This association measure is defined for zero-inflated,
@@ -24,9 +24,9 @@
 #' @return \code{numeric} weighted rho* association value between x and y
 #'
 #'
-#' scHOT 
-#' Ghazanfar, S., Lin, Y., Su, X. et al. Investigating higher-order interactions in 
-#'   single-cell data with scHOT. Nat Methods 17, 799–806 (2020). 
+#' scHOT
+#' Ghazanfar, S., Lin, Y., Su, X. et al. Investigating higher-order interactions in
+#'   single-cell data with scHOT. Nat Methods 17, 799–806 (2020).
 #'   https://doi.org/10.1038/s41592-020-0885-x
 #'
 #' Zero-Inflated Correlation
@@ -50,71 +50,70 @@ weightedZISpearman <- function(x, y, w = 1) {
     w <- rep(w, length(x))
   }
 
-  posx = x > 0
-  posy = y > 0
-  pospos = posx & posy
+  posx <- x > 0
+  posy <- y > 0
+  pospos <- posx & posy
 
-  p_11 = sum(w * pospos)/sum(w)
-  p_00 = sum(w * (!posx & !posy))/sum(w)
-  p_01 = sum(w * (!posx & posy))/sum(w)
-  p_10 = sum(w * (posx & !posy))/sum(w)
-  
+  p_11 <- sum(w * pospos) / sum(w)
+  p_00 <- sum(w * (!posx & !posy)) / sum(w)
+  p_01 <- sum(w * (!posx & posy)) / sum(w)
+  p_10 <- sum(w * (posx & !posy)) / sum(w)
 
-  if(any(pospos) & p_11 > 0){
-      rho_11 = wCorr::weightedCorr(x = x[pospos], y = y[pospos], weights = w[pospos], method = "Spearman")
-  }else{
-      print("Zero inflated Spearman correlation is undefined,
+
+  if (any(pospos) & p_11 > 0) {
+    rho_11 <- wCorr::weightedCorr(x = x[pospos], y = y[pospos], weights = w[pospos], method = "Spearman")
+  } else {
+    print("Zero inflated Spearman correlation is undefined,
           returning NA")
-      rho =NA
-      return(rho)    
+    rho <- NA
+    return(rho)
   }
-  
-  rho_star = p_11 * (p_01 + p_11) * (p_10 + p_11) * rho_11 +
-    3*(p_00 * p_11 - p_10 * p_01)
+
+  rho_star <- p_11 * (p_01 + p_11) * (p_10 + p_11) * rho_11 +
+    3 * (p_00 * p_11 - p_10 * p_01)
 
   if (is.na(rho_star)) {
     print("Zero inflated Spearman correlation is undefined,
           returning NA")
-    rho =NA
+    rho <- NA
     return(rho)
   }
-  
+
 
 
   return(rho_star)
 }
 
 
-##' scHOT functions, supersceded by used of wCorr's weighted correlation function, which backends to C for faster calculations.
-weightedSpearman = function(x, y, w = 1) {
-
-  if (length(x) != length(y)) {
-    stop("x and y should have the same length")
-  }
-  if (length(w) == 1) {
-    w <- rep(w, length(x))
-  }
-
-  keep = w > 0
-
-  xr = rank(x[keep])
-  yr = rank(y[keep])
-  return(weightedPearson(x = xr, y = yr, w = w[keep]))
-}
-
-weightedPearson = function(x, y, w = 1) {
-
-  if (length(x) != length(y)) stop("data must be the same length")
-
-  if (length(w) == 1) {
-    w <- rep(w, length(x))
-  }
-
-  nw = sum(w)
-  wssx = nw * sum(w * (x^2)) - sum(w * x)^2
-  wssy = nw * sum(w * (y^2)) - sum(w * y)^2
-  wssxy = nw * sum(w * x * y) - sum(w * x) * sum(w * y)
-  wcor = wssxy/sqrt(wssx * wssy)
-  return(wcor)
-}
-
+# ##' scHOT functions, supersceded by used of wCorr's weighted correlation function, which backends to C for faster calculations.
+# weightedSpearman = function(x, y, w = 1) {
+#
+#   if (length(x) != length(y)) {
+#     stop("x and y should have the same length")
+#   }
+#   if (length(w) == 1) {
+#     w <- rep(w, length(x))
+#   }
+#
+#   keep = w > 0
+#
+#   xr = rank(x[keep])
+#   yr = rank(y[keep])
+#   return(weightedPearson(x = xr, y = yr, w = w[keep]))
+# }
+#
+# weightedPearson = function(x, y, w = 1) {
+#
+#   if (length(x) != length(y)) stop("data must be the same length")
+#
+#   if (length(w) == 1) {
+#     w <- rep(w, length(x))
+#   }
+#
+#   nw = sum(w)
+#   wssx = nw * sum(w * (x^2)) - sum(w * x)^2
+#   wssy = nw * sum(w * (y^2)) - sum(w * y)^2
+#   wssxy = nw * sum(w * x * y) - sum(w * x) * sum(w * y)
+#   wcor = wssxy/sqrt(wssx * wssy)
+#   return(wcor)
+# }
