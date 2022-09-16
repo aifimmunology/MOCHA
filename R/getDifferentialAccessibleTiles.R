71dc97a9f2918ebd6a77e68dfd6777d48144542b
markphillippebworth <73675546+markphillippebworth@users.noreply.github.com>
2022-09-16 13:39:58 -0700

    Fixing ifelse : SampleTileMatrix error
    Needed to turn ifelse to if statement, because it was only return the first index of the sampletile  matrix instead of the entire sampletile matrix.

--- R/getDifferentialAccessibleTiles.R
+++ R/getDifferentialAccessibleTiles.R
@@ -79,7 +79,7 @@ getDifferentialAccessibleTiles <- function(SampleTileObj,
   ############################################################################
   # Estimate differential accessibility
   
-  sampleTileMatrix <- ifelse(Log2Intensity, sampleTileMatrix, log2(sampleTileMatrix+1))
+  if(Log2Intensity){ sampleTileMatrix <- log2(sampleTileMatrix+1) }
 
   res_pvals <- parallel::mclapply(rownames(sampleTileMatrix),
     function(x) {
