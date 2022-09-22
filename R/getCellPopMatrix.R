f4d77530ad2aafc668019a8456882ab9154c249d
Mark-Phillip Pebworth <markphillip.pebworth@alleninstitute.org>
2022-09-22 16:06:00 -0700

    Dropping Samples from getCellPopMatrix
    This function now has a flag that will drop samples if there were too few cells within them for accurate peak calling.

--- R/getCellPopMatrix.R
+++ R/getCellPopMatrix.R
@@ -5,16 +5,19 @@
 #'
 #' @param SampleTileObj The output from getSampleTileMatrix, a SummarizedExperiment of pseudobulk 
 #'   intensities across all tiles & cell types.
-#' @param @cellPopulation - The cell population you want to pull out.
-#'
+#' @param cellPopulation - The cell population you want to pull out.
+#' @param dropSamples , Boolean flag to determine whether to drop samples that were too small for peak calling. 
+#' 
 #' @return sampleTileMatrix a matrix of samples by called tiles for a given cell population.
 #'
 #' @export
-getCellPopMatrix <- function(SampleTileObj, cellPopulation){
+getCellPopMatrix <- function(SampleTileObj, cellPopulation, dropSamples = TRUE){
     
         tilesCalled <- GenomicRanges::mcols(SummarizedExperiment::rowRanges(SampleTileObj))[,cellPopulation]
         
         sampleTileMatrix <- SummarizedExperiment::assays(SampleTileObj)[[cellPopulation]][tilesCalled, , drop = FALSE]
-    
+		
+		if(dropSamples){sampleTileMatrix <- sampleTileMatrix[, colSums(sampleTileMatrix) > 0] }
+		
         return(sampleTileMatrix)
 }
