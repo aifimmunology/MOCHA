f2b09a9819d8baa1ceb051eea6db8586b5d4857c
imran-aifi <imran.mcgrath@alleninstitute.org>
2022-09-27 14:52:23 -0700

    FIX namespace in two_part

--- R/two_part.R
+++ R/two_part.R
@@ -33,10 +33,10 @@ TwoPart <- function(data, group, test = "wilcoxon", point.mass = 0) {
     B2 <- 0
   } else if ((success[1] == 0) | (success[2] == 0)) {
     T2 <- 0
-    B2 <- prop.test(pointmass, obs)$statistic
+    B2 <- stats::prop.test(pointmass, obs)$statistic
   } else if ((success[1] == 1) | (success[2] == 1)) {
     T2 <- 0
-    B2 <- prop.test(pointmass, obs)$statistic
+    B2 <- stats::prop.test(pointmass, obs)$statistic
   } else {
     uniq1 <- length(unique(Group1[Group1 != point.mass]))
     uniq2 <- length(unique(Group0[Group0 != point.mass]))
@@ -45,21 +45,21 @@ TwoPart <- function(data, group, test = "wilcoxon", point.mass = 0) {
       if (sum(pointmass) == 0) {
         B2 <- 0
       } else {
-        B2 <- prop.test(pointmass, obs)$statistic
+        B2 <- stats::prop.test(pointmass, obs)$statistic
       }
     } else if (sum(pointmass) == 0) {
       B2 <- 0
       if (test == "t.test") {
-        T2 <- t.test(data ~ group)$statistic^2
+        T2 <- stats::t.test(data ~ group)$statistic^2
       }
       if (test == "wilcoxon") {
-        W <- wilcox.test(data ~ group, exact = FALSE)$statistic
+        W <- stats::wilcox.test(data ~ group, exact = FALSE)$statistic
         mu <- (n1 * n2) / 2
         sigma <- sqrt((n1 * n2 * (n1 + n2 + 1)) / 12)
         T2 <- ((abs(W - mu) - 0.5) / sigma)^2
       }
     } else {
-      B2 <- prop.test(pointmass, obs)$statistic
+      B2 <- stats::prop.test(pointmass, obs)$statistic
       contIndex <- data != point.mass
       cont <- data[contIndex]
       cGroup <- group[contIndex]
@@ -67,10 +67,10 @@ TwoPart <- function(data, group, test = "wilcoxon", point.mass = 0) {
       n2c <- sum(cGroup == 0)
 
       if (test == "t.test") {
-        T2 <- t.test(cont ~ cGroup)$statistic^2
+        T2 <- stats::t.test(cont ~ cGroup)$statistic^2
       }
       if (test == "wilcoxon") {
-        W <- wilcox.test(cont ~ cGroup, exact = FALSE)$statistic
+        W <- stats::wilcox.test(cont ~ cGroup, exact = FALSE)$statistic
         mu <- (n1c * n2c) / 2
         sigma <- sqrt((n1c * n2c * (n1c + n2c + 1)) / 12)
         T2 <- ((abs(W - mu) - 0.5) / sigma)^2
@@ -79,9 +79,9 @@ TwoPart <- function(data, group, test = "wilcoxon", point.mass = 0) {
   }
   X2 <- B2 + T2
   if ((T2 == 0) | (B2 == 0)) {
-    X2pv <- 1 - pchisq(X2, 1)
+    X2pv <- 1 - stats::pchisq(X2, 1)
   } else {
-    X2pv <- 1 - pchisq(X2, 2)
+    X2pv <- 1 - stats::pchisq(X2, 2)
   }
   ans <- list(statistic = X2, pvalue = X2pv)
   return(ans)
