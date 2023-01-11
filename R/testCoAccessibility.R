0a4b537eb78020ef2291f4f6b08d049a7c0534f4
Mark-Phillip Pebworth <markphillip.pebworth@alleninstitute.org>
2023-01-11 12:13:30 -0800

    Create testCoAccessibility.R

---
+++ R/testCoAccessibility.R
@@ -0,0 +1,169 @@
+
+## tile1 and tile2 should be the numeric indices of the TSAM to test
+
+testCoAccessibility <- function(STObj, tile1, tile2, numCores = 1, ZI = TRUE, verbose = TRUE){
+
+    if(length(tile1) != length(tile2)){
+
+        error('tile1 and tile2 must be the same length.')
+
+    }
+
+    fullObj <- getBackGroundObj(STObj)
+
+    backPeaks <- chromVAR::getBackgroundPeaks(fullObj)
+
+    if(is.character(tile1) & is.character(tile2)){
+
+        nTile1 = match(tile1, rownames(fullObj))
+        nTile2 = match(tile1, rownames(fullObj))
+    }else if(is.numeric(tile1) & is.numeric(tile2)){
+
+        nTile1 = tile1
+        nTile2 = tile2
+
+        tile1 = rownames(fullObj)[nTile1]
+        tile2 = rownames(fullObj)[nTile2]
+
+    }else{error('tile1 and tile 2 must both be either numbers (indices) or strings')}
+
+
+    cl <- parallel::makeCluster(numCores)
+    parallel::clusterExport(cl, varList = c('nTile1', 'nTile2','backPeaks'), envir = environment())
+    
+    backgroundCombos <- pbapply::pblapply(seq_along(nTile1), function(x){
+            expand.grid(backPeaks[nTile1,], backPeaks[nTile2,])
+    }, cl = cl)
+
+    parallel::stopCluster()
+
+    cl <- parallel::makeCluster(numCores)
+
+    accMat <- SummarizedExperiment::assays(fullObj)[[1]]
+    
+    ## Test original pairs of locations
+    combPairs <- data.frame(tile1, tile2)
+    subAccMat <- accMat[unique(nTile1, nTile2),]
+
+    parallel::clusterExport(cl, varList = c('subAccMat', "combPairs"), envir = environment())
+    foreGround <- runCoAccessibility(subAccMat, combPairs, ZI, verbose, cl)
+    parallel::stopCluster()
+    ## Now we need to test the background set
+
+    backGround <- list()
+
+    for (i in 1:length(backgroundCombos)) {
+
+        subAccMat <- accMat[unique(nTile1, nTile2),]
+        combPairs <- backGroundCombos[[1]]
+        cl <- paralle::makeCluster(numCores)
+        message(paste('Generating Background correlations for', i, 'of', length(tile1), 'pairs', sep = " "))
+        parallel::clusterExport(cl, varList = c('subAccMat', "combPairs"), envir = environment())
+
+        tmp_background <- runCoAccessibility(subAccMat, combPairs, ZI, verbose, cl)
+        
+        parallel::stopCluster()
+
+        backGround <- append(backGround, tmp_background)
+    }
+
+
+
+
+
+
+}
+
+
+runCoAccessibility <- function(accMat, pairs, ZI = TRUE, verbose = TRUE, numCores = 1){
+
+    zero_inflated_spearman <- unlist(pbapply::pblapply(1:nrows(pairs),
+      function(x) {
+        weightedZISpearman(
+          x = accMat[pairs[x, 1], ],
+          y = accMat[pairs[x, 2], ],
+          verbose = verbose,
+          ZI = ZI
+        )
+      },
+      cl = numCores
+    ))
+
+    # Create zero-inflated correlation matrix from correlation values
+    zi_spear_mat_tmp <- data.table::data.table(
+      Correlation = zero_inflated_spearman,
+      Tile1 = pairs[, 1],
+      Tile2 = pairs[, 2]
+    )
+
+    return(zi_spear_mat_tmp)
+
+}
+
+
+getBackGroundObj <- function(STObj, NAtoZero = TRUE){
+
+  # Extract all the Sample-Tile Matrices for each cell type
+  temp <- SummarizedExperiment::assays(STObj)
+
+  meta1 <- SummarizedExperiment::colData(STObj)
+  
+  # Let's generate a new assay, that will contain the
+  # the intensity for a given cell, as well as the
+  # median intensity per sample-tile for all other cell types (i.e. the background)
+
+   	  
+
+  newAssays <- list(do.call('cbind', temp))
+  newSamplesNames <- unlist(lapply(names(temp), function(x){
+                            paste(x, colnames(STObj), sep = "__") %>% gsub(" ","",.)
+            }))
+
+  names(newAssays) <- 'counts'
+  colnames(newAssays[[1]]) = newSamplesNames
+
+  if(NAtoZero){  
+    newAssays[[1]][is.na(newAssays[[1]])] = 0 
+  }
+
+
+  allSampleData <- do.call('rbind', lapply(names(temp), function(x){
+
+		tmp_meta = meta1
+		 tmp_meta$Sample <- paste(x,tmp_meta$Sample, sep = "__") %>% gsub(" ","",.)
+                 tmp_meta$CellType = rep(x, dim(tmp_meta)[1])
+		 rownames(tmp_meta) <- tmp_meta$Sample
+		 tmp_meta
+	}
+  ))
+
+
+  
+  allRanges <- SummarizedExperiment::rowRanges(STObj)
+  for (i in names(temp)) {
+    mcols(allRanges)[, i] <- rep(TRUE, length(allRanges))
+  }
+
+  #return(list(newAssays, allSampleData, allRanges))
+
+  newObj <- SummarizedExperiment(
+    assays = newAssays,
+    colData =  allSampleData,
+    rowRanges = allRanges,
+    metadata = metadata(STObj)
+  )
+
+  newObj <- chromVAR::addGCBias(newObj, genome = metadata(STObj)$Genome)
+
+   if(any(is.na(SummarizedExperiment::rowData(newObj)$bias))){
+
+                    naList <- is.na(SummarizedExperiment::rowData(newObj)$bias)
+                    message(paste(sum(naList), "NaNs found within GC Bias", sep =" "))
+
+                    SummarizedExperiment::rowData(newObj)$bias[which(naList)] = mean(rowData(newObj)$bias, na.rm = TRUE)
+
+                }
+
+  return(newObj)
+
+}
