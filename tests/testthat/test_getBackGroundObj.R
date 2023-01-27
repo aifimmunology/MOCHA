064c25245fab5075144c45e41bbf2780acff01df
Imran McGrath <imran.mcgrath@alleninstitute.org>
2023-01-27 04:57:55 +0000

    TST tests for getBackGroundObj

--- tests/testthat/test_getBackGroundObj.R
+++ tests/testthat/test_getBackGroundObj.R
@@ -18,3 +18,24 @@ test_that("getBackGroundObj works on a 3-sample dataset", {
   
   }
 )
+
+test_that("getBackGroundObj works on a 1-sample dataset", {
+  
+  cellPopulations = c("C2", "C5")
+  capture.output(
+    STObj <- MOCHA::getSampleTileMatrix(
+      MOCHA:::testTileResults,
+      cellPopulations = cellPopulations,
+      threshold = 0
+    )
+  )
+
+  obj <- MOCHA::getBackGroundObj(STObj)
+  
+  expect_snapshot_output(
+    obj,
+    variant = "1sample"
+  )
+  
+  }
+)
