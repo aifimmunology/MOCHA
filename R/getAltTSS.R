025da34d21d8562cbd8152bdaa3cd5e7f849fe47
imran-aifi <imran.mcgrath@alleninstitute.org>
2023-02-09 17:56:59 -0800

    STY reflow comments and lines

--- R/getAltTSS.R
+++ R/getAltTSS.R
@@ -1,14 +1,32 @@
-#' @title \code{getAltTSS} Annotate Peaks falling in TSS Sites, and identify alternatively regulated TSSs for each gene. 
+#' @title \code{getAltTSS} Annotate Peaks falling in TSS Sites, and identify
+#'   alternatively regulated TSSs for each gene.
 #'
-#' @description \code{getAltTSS} Pulls out all peaks that fall in TSS sites, annotates them with the name of gene, and identifies genes that have evidence for alternatively regulated TSSs, 
-#'       including both type i (only some of the open TSSs for a gene are significantly more (or less) accessible), and type ii (multiple TSSs are significant different, with some being more accessible and others less).
-#'        Alternatively, this function  will return all open TSSs with differential measurements if the returnAllTSS flag is set to TRUE. 
+#' @description \code{getAltTSS} Pulls out all peaks that fall in TSS sites,
+#'   annotates them with the name of gene, and identifies genes that have
+#'   evidence for alternatively regulated TSSs, including both type i (only some
+#'   of the open TSSs for a gene are significantly more (or less) accessible),
+#'   and type ii (multiple TSSs are significant different, with some being more
+#'   accessible and others less). Alternatively, this function  will return all
+#'   open TSSs with differential measurements if the returnAllTSS flag is set to
+#'   TRUE.
 #'
-#' @param completeDAPs GRanges object that contains the differential measurements across all peaks (unfiltered DAPs). Will also work with data.frame or data.table version of a GRanges object. If you want alternatively regulated TSSs, the object must include a column names 'FDR', and 'Log2FC_C', which is standard for MOCHA differentials.
-#' @param returnAllTSS Flag to return all TSS sites with DAPs measurements, without filtering for alternative TSS usage. If multiple TSSs fall within the same tile, then that tile will be repeated for each TSS.
-#' @param nuancedTSS True/False flag to determine if alternative TSS genes should be filtered out if all their differential TSS usage falls within too small of a range. Default is TRUE
-#' @param nuancedTSSGAP Minimum distance betweeen TSSs needed for them to considered distinctly regulated TSSs. If two TSSs are too close, it is unclear and highly unlikely that ATAC data can distinguish between them. Default is 150 bp. 
-#' @param threshold FDR Threshold for determining significant vs non-significant changes in accessibility. Following MOCHA's standards, default is 0.2.
+#' @param completeDAPs GRanges object that contains the differential
+#'   measurements across all peaks (unfiltered DAPs). Will also work with
+#'   data.frame or data.table version of a GRanges object. If you want
+#'   alternatively regulated TSSs, the object must include a column names 'FDR',
+#'   and 'Log2FC_C', which is standard for MOCHA differentials.
+#' @param returnAllTSS Flag to return all TSS sites with DAPs measurements,
+#'   without filtering for alternative TSS usage. If multiple TSSs fall within
+#'   the same tile, then that tile will be repeated for each TSS.
+#' @param nuancedTSS True/False flag to determine if alternative TSS genes
+#'   should be filtered out if all their differential TSS usage falls within too
+#'   small of a range. Default is TRUE
+#' @param nuancedTSSGAP Minimum distance betweeen TSSs needed for them to
+#'   considered distinctly regulated TSSs. If two TSSs are too close, it is
+#'   unclear and highly unlikely that ATAC data can distinguish between them.
+#'   Default is 150 bp.
+#' @param threshold FDR Threshold for determining significant vs non-significant
+#'   changes in accessibility. Following MOCHA's standards, default is 0.2.
 #'
 #' @return tpeaks A GRanges containing annotated peaks falling in TSS sites.
 #'
@@ -22,13 +40,19 @@ getAltTSS <- function(completeDAPs,
                       TxDb = TxDb.Hsapiens.UCSC.hg38.refGene,
                       Org = org.Hs.eg.db) {
   . <- exactTSS <- name <- FDR <- Log2FC_C <- strand <- seqnames <- NULL
-  
+
   if (grepl("data.table|data.frame", class(completeDAPs)[1])) {
-    DAP_GRanges <- GenomicRanges::makeGRangesFromDataFrame(as.data.frame(completeDAPs), keep.extra.columns= TRUE)
+    DAP_GRanges <- GenomicRanges::makeGRangesFromDataFrame(
+      as.data.frame(completeDAPs),
+      keep.extra.columns = TRUE
+    )
   } else if (class(completeDAPs)[1] == "GRanges") {
     DAP_GRanges <- completeDAPs
   } else {
-    stop("completeDAPs object type is not compatible. Please submit a GRanges, data.table, or SummarizedExperiment object.")
+    stop(
+      "completeDAPs object type is not compatible. Please submit a GRanges",
+      ", data.table, or SummarizedExperiment object."
+    )
   }
 
   tss1 <- suppressWarnings(ensembldb::transcriptsBy(TxDb, by = ("gene")))
@@ -49,15 +73,22 @@ getAltTSS <- function(completeDAPs,
   if (returnAllTSS) {
     return(tpeaks)
   }
-  
-  if(!all(colnames(GenomicRanges::mcols(DAP_GRanges)) %in% c('FDR','Log2FC_C'))){
-  
-    stop('GRanges input does not include columns for FDR and Log2FC_C, and so alternatively regulated TSSs cannot be identified. Please provide an object with those columns for filtering.')
+
+  if (!all(
+    colnames(GenomicRanges::mcols(DAP_GRanges)) %in% c("FDR", "Log2FC_C")
+  )) {
+    stop(
+      "GRanges input does not include columns for FDR and Log2FC_C, so ",
+      "alternatively regulated TSSs cannot be identified. Please provide an",
+      " object with those columns for filtering."
+    )
   }
 
-  ## We want to select all the genes that have multiple TSSs (duplicated(name)) and at least one TSSs that has an FDR <= to the threshold.
-  ## After that, we want to filter for sites where only a subset of open TSSs change, or all TSSs change, but it opposite directions.
-  ###### may need to consider removing the exactTSS sites
+  # We want to select all the genes that have multiple TSSs (duplicated(name))
+  # and at least one TSSs that has an FDR <= to the threshold. After that, we
+  # want to filter for sites where only a subset of open TSSs change, or all
+  # TSSs change, but it opposite directions.
+  # may need to consider removing the exactTSS sites
   altTSS <- tpeaks %>%
     plyranges::filter(!duplicated(exactTSS)) %>%
     plyranges::group_by(name) %>%
@@ -65,9 +96,11 @@ getAltTSS <- function(completeDAPs,
 
 
   altTSS <- altTSS %>%
-    plyranges::filter(ifelse(all(FDR <= threshold, na.rm = TRUE) & !any(is.na(FDR)),
-      !all(Log2FC_C > 0) & !all(Log2FC_C < 0), TRUE
-    )) %>%
+    plyranges::filter(
+      ifelse(all(FDR <= threshold, na.rm = TRUE) & !any(is.na(FDR)),
+        !all(Log2FC_C > 0) & !all(Log2FC_C < 0), TRUE
+      )
+    ) %>%
     plyranges::ungroup() %>%
     sort()
 
@@ -77,7 +110,7 @@ getAltTSS <- function(completeDAPs,
         tmp <- IRanges::gaps(x) %>%
           filter(
             seqnames == GenomeInfoDb::seqnames(x) &
-            strand == GenomicRanges::strand(x)
+              strand == GenomicRanges::strand(x)
           ) %>%
           IRanges::width(.)
         ifelse(length(tmp) == 3 & tmp[2] < nuancedTSSGap, FALSE, TRUE)
