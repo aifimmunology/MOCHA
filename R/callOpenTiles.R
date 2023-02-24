7f555ae870afc47ac6a64dc5b92d74d0cf66f25f
Mark-Phillip Pebworth <markphillip.pebworth@alleninstitute.org>
2023-02-23 19:32:06 -0800

    Set everything to mclapply.

--- R/callOpenTiles.R
+++ R/callOpenTiles.R
@@ -234,7 +234,7 @@ setGeneric(
     # This pblapply will parallelize over each sample within a celltype.
     # Each arrow is a sample so this is allowed
     # (Arrow files are locked - one access at a time)
-    cl <- makeMOCHACluster(numCores)
+    cl <- makeMOCHACluster(numCores, parallelType = 'mclapply')
 
     tilesGRangesList <- pbapply::pblapply(
       cl = cl,
