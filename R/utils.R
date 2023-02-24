b119a1aa1722d9c600f003c2307bbd3b550cc251
Mark-Phillip Pebworth <markphillip.pebworth@alleninstitute.org>
2023-02-23 19:38:22 -0800

    Update utils.R

--- R/utils.R
+++ R/utils.R
@@ -67,7 +67,9 @@ makeMOCHACluster <- function(numCores = 1, type = NULL) {
 
   if (numCores > 1) {
     if(is.null(type) & .Platform$OS.type == "windows") {
+      
       cl <- parallel::makeCluster(numCores)
+
     } else if((is.null(type) & .Platform$OS.type != "windows")) {
 
       cl <- parallel::makeCluster(numCores, type = "FORK") # Use forking on unix
