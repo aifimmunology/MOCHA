eea15421bde01e2de69264f0e65c626103679d5e
Mark-Phillip Pebworth <markphillip.pebworth@alleninstitute.org>
2023-02-28 11:46:16 -0800

    Error in comparison
    Needed to make sure Tile1 and Tile2 weren't the same. It was testing for Tile1 and Tile1, which removed all indices of the background.

--- R/testCoAccessibility.R
+++ R/testCoAccessibility.R
@@ -302,7 +302,7 @@ testCoAccessibilityRandom <- function(STObj,
     }
     
     backgroundCombos = as.data.frame(backNumber)[,c('Tile1','Tile2')]
-    backgroundCombos <- backgroundCombos[backgroundCombos[, 'Tile1'] != backgroundCombos[,'Tile1'], ]
+    backgroundCombos <- backgroundCombos[backgroundCombos[, 'Tile1'] != backgroundCombos[,'Tile2'], ]
 
     if(dim(backNumber)[1] < 10){
       stop('User-defined background pairs are fewer than 10. Please provide a larger background.')
