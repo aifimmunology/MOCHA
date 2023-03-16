9c7896a6526a7c9d78eac5b807be5a9c939e956b
Mark-Phillip Pebworth <markphillip.pebworth@alleninstitute.org>
2023-03-16 10:18:40 -0700

    MinorEdits

---
+++ R/getCellTypeMarkers.R
@@ -0,0 +1,94 @@
+
+
+
+## Code that both runs a linear model across variables, and extracts the variance. This helps keep memory lower. 
+
+
+getCellTypeMarkers <- function(TSAM_Object, verbose = FALSE, numCores = 1){
+
+
+    fullObj <- combineSampleTileMatrix(TSAM_Object)
+
+    compCounts <- SummarizedExperiment::assays(fullObj)
+
+    metaData <- colData(fullObj)
+
+    #Generate formula
+    varForm <- paste0(unlist(lapply(variableList, function(x) paste('(1|',x,')',sep=''))), collapse = ' + ')
+    formula1 <- as.formula(paste('exp ~ ',varForm, sep = ''))
+
+    MetaDF <- dplyr::filter(as.data.frame(MetaDF), Sample %in% colnames(CountDF))
+    CountDF <- CountDF[,match(colnames(CountDF), MetaDF$Sample)]
+    
+    #Ensure that MetaDF and CountDF are aligned.
+    if(!all(colnames(CountDF) == MetaDF$Sample)){
+        stop('Samples in CountDF are not aligned with MetaDF')
+    }
+
+    #Ensure variable list is found in MetaDF
+    if(!all(variableList %in% colnames(MetaDF))){
+        stop('variableList includes variables not found in MetaDF.')
+    }
+
+    #Subset metadata to just the variables needed. This minimizes overhead for parallelization
+    MetaDF <- MetaDF[, colnames(MetaDF) %in% c('Sample', variableList)]
+
+    if(verbose){
+        message('Modeling results & Extracting Variance Decomposition')
+    }
+
+    # Make your clusters for efficient parallelization
+
+    cl <- parallel::makeCluster(numCores)
+    parallel::clusterExport(cl=cl, varlist = c('formula1', 'CountDF', 'MetaDF','runIndividualDecomposition'), 
+                        envir = environment())
+    decompList <- pbapply::pblapply(cl = cl, X = rownames(CountDF), runIndividualDecomposition)
+    parallel::stopCluster(cl)
+
+    varDecomp <- do.call('rbind', decompList)
+    colnames(varDecomp) <- c(variableList, 'Residual')
+    output_df <- cbind(data.frame(Variables = rownames(CountDF)), varDecomp)
+
+    return(output_df)
+}
+
+
+#' @title \code{runModeling}
+#'
+#' @description \code{runModeling} Runs linear modeling on data provided. Written for efficient parallelization.
+#'
+#' @param refList. A list where the first index is a data.frame to use for modeling, and the second is the formula for modeling.  
+#'
+#' @return A linear model
+
+#' @export
+#' 
+#' 
+runIndividualDecomposition <- function(x){
+
+    df <-  data.frame(exp = as.numeric(CountDF[x,]), 
+                MetaDF, stringsAsFactors = FALSE)
+
+    modelRes <- tryCatch({lme4::lmer(formula = formula1, data =  df)}, 
+                                error = function(e){NA})
+    variableList <-  all.vars(formula1[-1])
+
+    output_vector <- tryCatch({
+        if(!is.na(modelRes)){
+            ## Extract variance decomposition.
+            lmem_re <- as.data.frame(lme4::VarCorr(modelRes))
+            row.names(lmem_re) <- c(lmem_re$grp)
+        
+            lmem_re <- lmem_re[c(variableList,'Residual'), ]
+
+            VarDecomp <- (lmem_re$vcov)/sum(lmem_re$vcov)
+
+        }else{
+            VarDecomp <- rep(NA, length(variableList) + 1)
+        }
+
+        VarDecomp
+    }, 
+      error = function(e){rep(NA, length(variableList) + 1)})
+    return(output_vector)
+}
