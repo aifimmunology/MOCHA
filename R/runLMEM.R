b923c82a1f90748406c9d12f1c283e6fa61706c3
Mark-Phillip Pebworth <markphillip.pebworth@alleninstitute.org>
2023-03-23 09:20:22 -0700

    Updates to MOCHA modeling

--- R/runLMEM.R
+++ R/runLMEM.R
@@ -6,13 +6,14 @@
 
 ## formula must be in the form exp ~ <variables>
 
-
+## Initial sampling: When the model fails to converge, it needs to return a default data.frame with NAs that is the same size. 
+## The initial sampling provides a blueprint for how big that NA data.frame should be. The default is that it'll run 5 models, and if they are all NA, through an error. 
 ## This function should only be used on continuous, non-zero inflated data. 
 
-runModeling <- function(TSAM_Object,
+runLMEM <- function(TSAM_Object,
                         cellTypeName = NULL,
                         modelFormula = NULL, 
-                        ZI = FALSE,
+                        initialSampling = 5,
                         verbose = FALSE, 
                         numCores = 1){
 
@@ -21,8 +22,8 @@ runModeling <- function(TSAM_Object,
         stop("No cell type name was provided.")
     }else if(length(cellTypeName) > 1){
         stop("Please provide only one string within cellTypeName. If you want to run over multiple cell types, please use combineSampleTileMatrix() to generate a new object, and use that object instead, with cellTypeName = 'counts'")
-    }else if(!cellTypeName %in% names(SummarizedExperiment::assay(TSAM_Object))){
-        stop("No cell type name not found within TSAM_Object.")
+    }else if(!cellTypeName %in% names(SummarizedExperiment::assays(TSAM_Object))){
+        stop("cellTypeName was not found within TSAM_Object.")
     }
 
     modelingData <- as.data.frame(getCellPopMatrix(TSAM_Object, cellPopulation = cellTypeName, NAtoZero = TRUE))
@@ -30,7 +31,7 @@ runModeling <- function(TSAM_Object,
 
     if (is.null(modelFormula)) {
         stop("No formula was provided.")
-    }else if(all(all.vars(modelFormula) %in% c('~','exp',colnames(metaData)))){
+    }else if(!all(all.vars(modelFormula) %in% c('~','exp',colnames(MetaDF)))){
         stop("Model formula is not in the correct format (exp ~ factors) or model factors are not found in column names of metadata within the TSAM_Object.")
     }
 
@@ -43,6 +44,36 @@ runModeling <- function(TSAM_Object,
     #Subset metadata to just the variables needed. This minimizes overhead for parallelization
     MetaDF <- MetaDF[, colnames(MetaDF) %in% c('Sample', variableList)]
 
+    
+    if(verbose){
+        message('Running a quick test.')
+    }
+
+
+    #Generate pilot data for the null data.frame
+    pilotIndices <- sample(x = 1:dim(modelingData)[1], size= initialSampling, replace = FALSE)
+    modelList <- pbapply::pblapply(X = pilotIndices, function(x){
+
+        df <-  data.frame(exp = as.numeric(modelingData[x,]), 
+                MetaDF, stringsAsFactors = FALSE)
+
+        tryCatch({
+            lmerTest::lmer(formula = modelFormula, data =  df)
+        }, error = function(e){ NA})
+
+    }, cl = NULL)
+
+    if(all(is.na(unlist(modelList)))){
+        stop('For the initial sampling, every test model failed. Reconsider modelFormula or increase initial sampling size.')
+    }else{
+
+        idx <- which(!is.na(unlist(modelList)))
+        nullDF <- as.data.frame(summary(modelList[[idx[1]]])$coefficients)
+        nullDF[!is.na(nullDF)] <- NA
+        rm(modelList)
+    }
+
+    
     if(verbose){
         message('Modeling results.')
     }
@@ -50,13 +81,13 @@ runModeling <- function(TSAM_Object,
     # Make your clusters for efficient parallelization
 
     cl <- parallel::makeCluster(numCores)
-    parallel::clusterExport(cl=cl, varlist = c('modelFormula', 'modelingData', 'MetaDF','individualModel', 'fixedEffects'), 
+    parallel::clusterExport(cl=cl, varlist = c('modelFormula', 'modelingData', 'MetaDF','individualLMEM','nullDF'), 
                         envir = environment())
-    coeffList <- pbapply::pblapply(cl = cl, X = rownames(modelingData), individualModel)
+    coeffList <- pbapply::pblapply(cl = cl, X = rownames(modelingData), individualLMEM)
     parallel::stopCluster(cl)
 
     if(verbose){
-        message('Modeling results.')
+        message('Reorganizing coefficients.')
     }
 
     slopes <-  do.call('rbind', pbapply::pblapply(X = coeffList, function(x){ 
@@ -79,6 +110,7 @@ runModeling <- function(TSAM_Object,
                 error_tmp
 
     },cl = NULL))
+
     rownames(stdError) <- rownames(significance) <- rownames(slopes) <- rownames(modelingData)
 
 
@@ -88,9 +120,9 @@ runModeling <- function(TSAM_Object,
 }
 
 
-#' @title \code{IndividualModel}
+#' @title \code{IndividualLMEM}
 #'
-#' @description \code{IndividualModel} Runs linear modeling on data provided. Written for efficient parallelization.
+#' @description \code{IndividualLMEM} Runs linear modeling on data provided. Written for efficient parallelization.
 #'
 #' @param refList. A list where the first index is a data.frame to use for modeling, and the second is the formula for modeling.  
 #'
@@ -99,20 +131,16 @@ runModeling <- function(TSAM_Object,
 #' @export
 #' 
 #' 
-individualModel <- function(x){
+individualLMEM <- function(x){
 
     df <-  data.frame(exp = as.numeric(modelingData[x,]), 
                 MetaDF, stringsAsFactors = FALSE)
 
     output_vector <- tryCatch({
-
         modelRes <- lmerTest::lmer(formula = modelFormula, data =  df)
         as.data.frame(summary(modelRes)$coefficients)
     }, error = function(e){
-            emptyMat <- as.data.frame(matrix(NA, nrow = length(fixedEffects) + 1, ncol = 5))
-            colnames(emptyMat) <-  c('Estimate', 'Std. Error', 'df', 't value', 'Pr(>|t|)')
-            rownames(emptyMat) <- c('(Intercept)', fixedEffects)
-            emptyMat
+            nullDF
       })
     return(output_vector)
 }
@@ -121,7 +149,7 @@ individualModel <- function(x){
 ## Code for testing out formulas on the data. Runs a given formula on a subset of the data, and returns the model results. 
 ## This is meant to help during the model selection process. 
 
-pilotModel <- function(TSAM_Object,
+pilotLMEM <- function(TSAM_Object,
                         cellTypeName = NULL,
                         modelFormula = NULL, 
                         ZI = FALSE,
@@ -132,8 +160,8 @@ pilotModel <- function(TSAM_Object,
         stop("No cell type name was provided.")
     }else if(length(cellTypeName) > 1){
         stop("Please provide only one string within cellTypeName. If you want to run over multiple cell types, please use combineSampleTileMatrix() to generate a new object, and use that object instead, with cellTypeName = 'counts'")
-    }else if(!cellTypeName %in% names(SummarizedExperiment::assay(TSAM_Object))){
-        stop("No cell type name not found within TSAM_Object.")
+    }else if(!cellTypeName %in% names(SummarizedExperiment::assays(TSAM_Object))){
+        stop("cellTypeName was not found within TSAM_Object.")
     }
 
     modelingData <- as.data.frame(getCellPopMatrix(TSAM_Object, cellPopulation = cellTypeName, NAtoZero = TRUE))
@@ -141,7 +169,7 @@ pilotModel <- function(TSAM_Object,
 
     if (is.null(modelFormula)) {
         stop("No formula was provided.")
-    }else if(all(all.vars(modelFormula) %in% c('~','exp',colnames(metaData)))){
+    }else if(!all(all.vars(modelFormula) %in% c('~','exp',colnames(MetaDF)))){
         stop("Model formula is not in the correct format (exp ~ factors) or model factors are not found in column names of metadata within the TSAM_Object.")
     }
 
@@ -160,16 +188,13 @@ pilotModel <- function(TSAM_Object,
 
     # Make your clusters for efficient parallelization
 
-    modelList <- lapply(pilotIndices, function(x){
+    modelList <- pbapply::pblapply(X = pilotIndices, function(x){
 
         df <-  data.frame(exp = as.numeric(modelingData[x,]), 
                 MetaDF, stringsAsFactors = FALSE)
 
-        modelRes <- tryCatch({
-            lmerTest::lmer(formula = modelFormula, data =  df)
-        }, error = function(e){ NA  })
-        modelRes
-    })
+        lmerTest::lmer(formula = modelFormula, data =  df)
+    }, cl = NULL)
 
     return(modelList)
 }
---
+++ R/runZIGLMM.R
@@ -0,0 +1,225 @@
+## To do: Implement ZI options, and figure out how to handle Log2 or not log2. 
+
+## Will take data (metadata and modeling data), and use it to model a given formula.
+## It then returns a data.frame of intercept, slope, significance, and (residual?) 
+## for each row of modelingData
+
+## formula must be in the form exp ~ <variables>
+
+#Exa
+
+runZIGLMM <- function(TSAM_Object,
+                        cellTypeName = NULL,
+                        continuousFormula = NULL, 
+                        ziformula = NULL,
+                        initialSampling = 5,
+                        verbose = FALSE, 
+                        numCores = 1){
+
+    if(any(c(class(continuousFormula), class(ziformula)) != 'formula')){
+        stop('continuousFormula and/or ziformula was not provided as a formula.')
+    }
+    
+    if (is.null(cellTypeName)) {
+        stop("No cell type name was provided.")
+    }else if(length(cellTypeName) > 1){
+        stop("Please provide only one string within cellTypeName. If you want to run over multiple cell types, please use combineSampleTileMatrix() to generate a new object, and use that object instead, with cellTypeName = 'counts'")
+    }else if(!cellTypeName %in% names(SummarizedExperiment::assays(TSAM_Object))){
+        stop("No cell type name not found within TSAM_Object.")
+    }
+
+    modelingData <- as.data.frame(getCellPopMatrix(TSAM_Object, cellPopulation = cellTypeName, NAtoZero = TRUE))
+    MetaDF <- as.data.frame(SummarizedExperiment::colData(TSAM_Object))
+
+    if(!all(all.vars(continuousFormula) %in% c('exp',colnames(MetaDF)))){
+        stop("Model formula is not in the correct format (exp ~ factors) or model factors are not found in column names of metadata within the TSAM_Object.")
+    }
+
+    if(!all(all.vars(ziformula) %in% colnames(MetaDF))){
+        stop("factors from the ziformula were not found in the metadata.")
+    }
+
+    variableList <- c(all.vars(continuousFormula)[all.vars(continuousFormula) != 'exp'], all.vars(ziformula))
+
+    MetaDF <- dplyr::filter(MetaDF, Sample %in% colnames(modelingData))
+    modelingData <- modelingData[,match(colnames(modelingData), MetaDF$Sample)]
+    
+
+    #Subset metadata to just the variables needed. This minimizes overhead for parallelization
+    MetaDF <- MetaDF[, colnames(MetaDF) %in% c('Sample', variableList)]
+
+     
+    if(verbose){
+        message('Running a quick test.')
+    }
+
+
+    #Generate pilot data for the null data.frame
+    pilotIndices <- sample(x = 1:dim(modelingData)[1], size= initialSampling, replace = FALSE)
+    modelList <- pbapply::pblapply(X = pilotIndices, function(x){
+
+        df <-  data.frame(exp = as.numeric(modelingData[x,]), 
+                MetaDF, stringsAsFactors = FALSE)
+
+        tryCatch({
+            glmmTMB::glmmTMB(continuousFormula,
+                                ziformula = ziformula,
+                                data=df,
+                                family = gaussian(),
+                                REML=TRUE)
+        }, error = function(e){ NA})
+
+    }, cl = NULL)
+
+    if(all(is.na(unlist(modelList)))){
+        stop('For the initial sampling, every test model failed. Reconsider modelFormula or increase initial sampling size.')
+    }else{
+
+        idx <- which(!is.na(unlist(modelList)))
+        nullDF <- lapply(summary(modelList[[idx[1]]])$coefficients, as.data.frame)
+        nullDF$cond[!is.na(nullDF$cond)] <- NA
+        nullDF$zi[!is.na(nullDF$zi)] <- NA
+        rm(modelList)
+    }
+
+    if(verbose){
+        message('Modeling results.')
+    }
+
+
+    # Make your clusters for efficient parallelization
+
+    cl <- parallel::makeCluster(numCores)
+    parallel::clusterEvalQ(cl, {library(glmmTMB)})
+    parallel::clusterExport(cl=cl, varlist = c('continuousFormula', 'modelingData', 'MetaDF','individualZIGLMM','nullDF'), 
+                        envir = environment())
+    coeffList <- pbapply::pblapply(cl = cl, X = rownames(modelingData), individualZIGLMM)
+    parallel::stopCluster(cl)
+    return(coeffList)
+    output_list <- lapply(list('cond','zi'), function(y){
+        
+        if(verbose){
+            message(stringr::str_interp("Extracting coefficients for the ${y} component"))
+        }
+
+        slopes <-  do.call('rbind', pbapply::pblapply(X = coeffList, function(x){ 
+                    slope_tmp <- x[[y]]$Estimate
+                    names(slope_tmp) <- rownames(x[[y]])
+                    slope_tmp
+
+        },cl = NULL))
+
+        significance <-  do.call('rbind', pbapply::pblapply(X = coeffList, function(x){ 
+                    sig_tmp <- x[[y]]$'Pr(>|z|)'
+                    names(sig_tmp) <- rownames(x[[y]])
+                    sig_tmp
+
+        },cl = NULL))
+
+        stdError <-  do.call('rbind', pbapply::pblapply(X = coeffList, function(x){ 
+                    error_tmp <- x[[y]]$'Std. Error'
+                    names(error_tmp) <- rownames(x)
+                    error_tmp
+
+        },cl = NULL))
+        rownames(stdError) <- rownames(significance) <- rownames(slopes) <- rownames(modelingData)
+        list('Slopes' = slopes, 'Significance' = significance, 'StdError' = stdError)
+    })
+    names(output_list) <- c('cond','zi')
+
+    return(output_list)
+}
+
+
+#' @title \code{IndividualLMEM}
+#'
+#' @description \code{IndividualLMEM} Runs linear modeling on data provided. Written for efficient parallelization.
+#'
+#' @param refList. A list where the first index is a data.frame to use for modeling, and the second is the formula for modeling.  
+#'
+#' @return A linear model
+
+#' @export
+#' 
+#' 
+individualZIGLMM <- function(x){
+
+    df <-  data.frame(exp = as.numeric(modelingData[x,]), 
+                MetaDF, stringsAsFactors = FALSE)
+
+     output_vector <- tryCatch({
+        modelRes <-glmmTMB::glmmTMB(continuousFormula,
+                                ziformula = ziformula,
+                                data=df,
+                                family = stats::gaussian(),
+                                REML=TRUE)
+        lapply(summary(modelRes)$coefficients, as.data.frame)
+    }, error = function(e){nullDF})
+    return(output_vector)
+}
+
+
+## Code for testing out formulas on the data. Runs a given formula on a subset of the data, and returns the model results. 
+## This is meant to help during the model selection process. 
+
+#example: pilotZIGLMM(STM, 'CD16 Mono',exp~ Age + Sex + days_since_symptoms + (1|PTID), ~ 0, verbose = TRUE )
+
+pilotZIGLMM <- function(TSAM_Object,
+                        cellTypeName = NULL,
+                        continuousFormula = NULL, 
+                        ziformula = NULL,
+                        verbose = FALSE,
+                        pilotIndices = 1:10){
+
+    if(any(c(class(continuousFormula), class(ziformula)) != 'formula')){
+        stop('continuousFormula and/or ziformula was not provided as a formula.')
+    }
+    
+    if (is.null(cellTypeName)) {
+        stop("No cell type name was provided.")
+    }else if(length(cellTypeName) > 1){
+        stop("Please provide only one string within cellTypeName. If you want to run over multiple cell types, please use combineSampleTileMatrix() to generate a new object, and use that object instead, with cellTypeName = 'counts'")
+    }else if(!cellTypeName %in% names(SummarizedExperiment::assays(TSAM_Object))){
+        stop("Cell type name not found within TSAM_Object.")
+    }
+
+    modelingData <- as.data.frame(getCellPopMatrix(TSAM_Object, cellPopulation = cellTypeName, NAtoZero = TRUE))
+    MetaDF <- as.data.frame(SummarizedExperiment::colData(TSAM_Object))
+
+    if(!all(all.vars(continuousFormula) %in% c('exp',colnames(MetaDF)))){
+        stop("Model formula is not in the correct format (exp ~ factors) or model factors are not found in column names of metadata within the TSAM_Object.")
+    }
+
+    if(!all(all.vars(ziformula) %in% c(colnames(MetaDF))) & length(all.vars(ziformula)) > 0){
+        stop("factors from the ziformula were not found in the metadata.")
+    }
+
+    variableList <- c(all.vars(continuousFormula)[all.vars(continuousFormula) != 'exp'], all.vars(ziformula))
+
+    MetaDF <- dplyr::filter(MetaDF, Sample %in% colnames(modelingData))
+    modelingData <- modelingData[pilotIndices,match(colnames(modelingData), MetaDF$Sample)]
+    
+
+    #Subset metadata to just the variables needed. This minimizes overhead for parallelization
+    MetaDF <- MetaDF[, colnames(MetaDF) %in% c('Sample', variableList)]
+
+    if(verbose){
+        message('Modeling results.')
+    }
+
+    # Make your clusters for efficient parallelization
+
+    modelList <- pbapply::pblapply(X = pilotIndices, function(x){
+
+        df <-  data.frame(exp = as.numeric(modelingData[x,]), 
+                MetaDF, stringsAsFactors = FALSE)
+
+        glmmTMB::glmmTMB(continuousFormula,
+                                ziformula = ziformula,
+                                data=df,
+                                family = stats::gaussian(),
+                                REML=TRUE)
+    }, cl = NULL)
+
+    return(modelList)
+}
