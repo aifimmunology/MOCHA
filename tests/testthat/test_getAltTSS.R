e2727133e1c3a96f81f5b0006694f44a3b23bfab
imran-aifi <imran.mcgrath@alleninstitute.org>
2023-05-02 10:24:57 -0700

    TST update getAltTSS tests and snapshots after validating reproducibility

--- tests/testthat/test_getAltTSS.R
+++ tests/testthat/test_getAltTSS.R
@@ -1,112 +1,118 @@
-test_that("getAltTSS works with different input formats", {
-  
-  testPeakDT <- MOCHA:::testPeaks
-  set.seed(2023)
-  testPeakDT$Log2FC_C <- sample(seq(-4, 4, .1), nrow(testPeakDT), replace=TRUE)
-  
-  testPeakDF <- as.data.frame(testPeakDT)
-  
-  testPeakGR <- MOCHA::differentialsToGRanges(
-    testPeakDT, tileColumn = "tileID"
-  )
-  
-  # This will drop these from metadata: tileID seqnames start end strand 
-  testPeakGR2 <- GenomicRanges::makeGRangesFromDataFrame(
-    as.data.frame(testPeakDT),
-    keep.extra.columns = TRUE
-  )
-  
-  expect_true(all(
-    nrow(testPeakDF) == nrow(testPeakDT),
-    nrow(testPeakDF) == length(testPeakGR)
-  ))
-  
-  # data.table
-  tpeaksDT <- getAltTSS(
-    completeDAPs = testPeakDT,
-    returnAllTSS = FALSE,
-    nuancedTSS = FALSE,
-    nuancedTSSGap = 150,
-    threshold = 0.2,
-    TxDb = TxDb.Hsapiens.UCSC.hg38.refGene,
-    Org = org.Hs.eg.db
-  )
-  
-  # data.frame
-  tpeaksDF <- getAltTSS(
-    completeDAPs = testPeakDF,
-    returnAllTSS = FALSE,
-    nuancedTSS = FALSE,
-    nuancedTSSGap = 150,
-    threshold = 0.2,
-    TxDb = TxDb.Hsapiens.UCSC.hg38.refGene,
-    Org = org.Hs.eg.db
-  )
-  
-  # GRanges
-  tpeaksGR <- getAltTSS(
-    completeDAPs = testPeakGR,
-    returnAllTSS = FALSE,
-    nuancedTSS = FALSE,
-    nuancedTSSGap = 150,
-    threshold = 0.2,
-    TxDb = TxDb.Hsapiens.UCSC.hg38.refGene,
-    Org = org.Hs.eg.db
-  )
-  
-  expect_true(all(
-    length(tpeaksDT) == length(tpeaksDF),
-    length(tpeaksDT) == length(tpeaksGR)
-  ))
-  
-  expect_snapshot_output(
-    tpeaksDT
-  )
-  
-})
+if (
+  require("TxDb.Hsapiens.UCSC.hg38.refGene") &&
+  require("org.Hs.eg.db")
+) {
+  test_that("getAltTSS works with different input formats", {
 
-test_that("getAltTSS works with returnAllTSS = TRUE", {
-  
-  testPeakDT <- MOCHA:::testPeaks
-  set.seed(2023)
-  testPeakDT$Log2FC_C <- sample(seq(-4, 4, .1), nrow(testPeakDT), replace=TRUE)
-  
-  tpeaks <- getAltTSS(
-    completeDAPs = testPeakDT,
-    returnAllTSS = TRUE,
-    nuancedTSS = FALSE,
-    nuancedTSSGap = 150,
-    threshold = 0.2,
-    TxDb = TxDb.Hsapiens.UCSC.hg38.refGene,
-    Org = org.Hs.eg.db
-  )
-  
-  expect_snapshot_output(
-    tpeaks,
-    variant = "returnAllTSS"
-  )
-  
-})
+    testPeakDT <- MOCHA:::testPeaks
+    set.seed(2023)
+    testPeakDT$Log2FC_C <- sample(seq(-4, 4, .1), nrow(testPeakDT), replace=TRUE)
 
-test_that("getAltTSS works with nuancedTSS = TRUE", {
-  
-  testPeakDT <- MOCHA:::testPeaks
-  set.seed(2023)
-  testPeakDT$Log2FC_C <- sample(seq(-4, 4, .1), nrow(testPeakDT), replace=TRUE)
-  
-  tpeaksDT <- getAltTSS(
-    completeDAPs = testPeakDT,
-    returnAllTSS = FALSE,
-    nuancedTSS = TRUE,
-    nuancedTSSGap = 150,
-    threshold = 0.2,
-    TxDb = TxDb.Hsapiens.UCSC.hg38.refGene,
-    Org = org.Hs.eg.db
-  )
-  
-  expect_snapshot_output(
-    tpeaksDT,
-    variant = "nuancedTSS"
-  )
-  
-})
+    testPeakDF <- as.data.frame(testPeakDT)
+
+    testPeakGR <- MOCHA::differentialsToGRanges(
+      testPeakDT, tileColumn = "tileID"
+    )
+
+    # This will drop these from metadata: tileID seqnames start end strand
+    testPeakGR2 <- GenomicRanges::makeGRangesFromDataFrame(
+      as.data.frame(testPeakDT),
+      keep.extra.columns = TRUE
+    )
+
+    expect_true(all(
+      nrow(testPeakDF) == nrow(testPeakDT),
+      nrow(testPeakDF) == length(testPeakGR)
+    ))
+
+    # data.table
+    tpeaksDT <- getAltTSS(
+      completeDAPs = testPeakDT,
+      returnAllTSS = FALSE,
+      nuancedTSS = FALSE,
+      nuancedTSSGap = 150,
+      threshold = 0.2,
+      TxDb = TxDb.Hsapiens.UCSC.hg38.refGene,
+      Org = org.Hs.eg.db
+    )
+
+    # data.frame
+    tpeaksDF <- getAltTSS(
+      completeDAPs = testPeakDF,
+      returnAllTSS = FALSE,
+      nuancedTSS = FALSE,
+      nuancedTSSGap = 150,
+      threshold = 0.2,
+      TxDb = TxDb.Hsapiens.UCSC.hg38.refGene,
+      Org = org.Hs.eg.db
+    )
+
+    # GRanges
+    tpeaksGR <- getAltTSS(
+      completeDAPs = testPeakGR,
+      returnAllTSS = FALSE,
+      nuancedTSS = FALSE,
+      nuancedTSSGap = 150,
+      threshold = 0.2,
+      TxDb = TxDb.Hsapiens.UCSC.hg38.refGene,
+      Org = org.Hs.eg.db
+    )
+
+    expect_true(all(
+      length(tpeaksDT) == length(tpeaksDF),
+      length(tpeaksDT) == length(tpeaksGR)
+    ))
+
+    expect_snapshot_output(
+      tpeaksDT
+    )
+
+  })
+
+  test_that("getAltTSS works with returnAllTSS = TRUE", {
+
+    testPeakDT <- MOCHA:::testPeaks
+    set.seed(2023)
+    testPeakDT$Log2FC_C <- sample(seq(-4, 4, .1), nrow(testPeakDT), replace=TRUE)
+
+    tpeaks <- getAltTSS(
+      completeDAPs = testPeakDT,
+      returnAllTSS = TRUE,
+      nuancedTSS = FALSE,
+      nuancedTSSGap = 150,
+      threshold = 0.2,
+      TxDb = TxDb.Hsapiens.UCSC.hg38.refGene,
+      Org = org.Hs.eg.db
+    )
+
+    expect_snapshot_output(
+      tpeaks,
+      variant = "returnAllTSS"
+    )
+
+  })
+
+  test_that("getAltTSS works with nuancedTSS = TRUE", {
+
+    testPeakDT <- MOCHA:::testPeaks
+    set.seed(2023)
+    testPeakDT$Log2FC_C <- sample(seq(-4, 4, .1), nrow(testPeakDT), replace=TRUE)
+
+    tpeaksDT <- getAltTSS(
+      completeDAPs = testPeakDT,
+      returnAllTSS = FALSE,
+      nuancedTSS = TRUE,
+      nuancedTSSGap = 150,
+      threshold = 0.2,
+      TxDb = TxDb.Hsapiens.UCSC.hg38.refGene,
+      Org = org.Hs.eg.db
+    )
+
+    expect_snapshot_output(
+      tpeaksDT,
+      variant = "nuancedTSS"
+    )
+
+  })
+}
+
