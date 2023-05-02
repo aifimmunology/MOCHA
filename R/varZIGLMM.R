8dd8d37b6053101b0c37a117b8cc2e3dc116f879
Mark-Phillip Pebworth <markphillip.pebworth@alleninstitute.org>
2023-05-02 13:48:42 -0700

    BUG: Fixing varZIGLMM
    Working version

--- R/varZIGLMM.R
+++ R/varZIGLMM.R
@@ -1,6 +1,34 @@
-
-
-
+#' @title Zero-inflated Variance Decomposition for pseudobulked scATAC data
+#'
+#' @description \code{varZIGLMM} Identified variance decomposition on a given cell type across both zero-inflated and continous
+#'  space using a zero-inflated general linear mixed model \code{\link[glmmTMB]{glmmTMB}}
+#'
+#' @param TSAM_Object A SummarizedExperiment object generated from
+#'   getSampleTileMatrix. 
+#' @param cellTypeName Name of a cell type. Should match up exactly with the assay name within SummarizedExperiment. 
+#' @param continuousRandom Random effects to test in the continuous portion. All factors must be found in column names
+#'   of the TSAM_Object metadata, except for FragNumber and CellCount, which will be extracted from the TSAM_Object's metadata.
+#' @param ziformula Random effects to test in the zero-inflated portion. All factors must be found in column names
+#'   of the TSAM_Object colData metadata, except for FragNumber and CellCount, which will be extracted from the TSAM_Object's metadata.
+#' @param verbose Set TRUE to display additional messages. Default is FALSE.
+#' @param numCores integer. Number of cores to parallelize across.
+#'
+#' @return results a SummarizedExperiment containing LMEM results
+#'
+#'
+#'
+#' @examples
+#' \dontrun{
+#'   modelList <- runZIGLMM(STM[c(1:1000),], 
+#'                  cellTypeName = 'CD16 Mono',
+#'                  continuousRandom = c('Age', 'Sex', 'Days'),
+#'                  ziRando = c('FragNumber', 'Days'),
+#'                  verbose = TRUE, 
+#'                  numCores = 35 )
+#' }
+#'
+#' @export
+#' 
 varZIGLMM <- function(TSAM_Object,
                       cellTypeName = NULL,
                       continuousRandom = NULL,
@@ -16,7 +44,7 @@ varZIGLMM <- function(TSAM_Object,
   } else if (!cellTypeName %in% names(SummarizedExperiment::assays(TSAM_Object))) {
     stop("No cell type name not found within TSAM_Object.")
   }
-
+ 
   newObj <- combineSampleTileMatrix(subsetMOCHAObject(TSAM_Object, subsetBy = 'celltype', groupList = cellTypeName, subsetPeaks = TRUE))
   modelingData <- log2(SummarizedExperiment::assays(newObj)[['counts']]+1)
   MetaDF <- as.data.frame(SummarizedExperiment::colData(newObj))
@@ -30,7 +58,7 @@ varZIGLMM <- function(TSAM_Object,
   }
 
   CondvarForm <- paste0(unlist(lapply(continuousRandom, function(x) paste('(1|',x,')',sep=''))), collapse = ' + ')
-  continuousFormula <- as.formula(paste('exp ~ ',CondvarForm, sep = ''))
+  continuousFormula <- paste('exp ~ ',CondvarForm, sep = '')
 
   if(all(ziRandom != 0)){
     zi_form <- paste0(unlist(lapply(ziRandom, function(x) paste('(1|',x,')',sep=''))), collapse = ' + ')
@@ -43,6 +71,7 @@ varZIGLMM <- function(TSAM_Object,
 
   MetaDF <- dplyr::filter(MetaDF, Sample %in% colnames(modelingData))
   modelingData <- modelingData[, match(colnames(modelingData), MetaDF$Sample)]
+<<<<<<< Updated upstream
 
   # Subset metadata to just the variables needed. This minimizes overhead for parallelization
   MetaDF <- MetaDF[, colnames(MetaDF) %in% c("Sample", variableList)]
@@ -51,6 +80,8 @@ varZIGLMM <- function(TSAM_Object,
     stop("NAs are included in the MetaDF. Please remove them and try again.")
   }
 
+=======
+>>>>>>> Stashed changes
 
   #Generate null results.
   if(all(ziRandom != 0)){
@@ -61,34 +92,50 @@ varZIGLMM <- function(TSAM_Object,
     names(nullDF) <- c(paste('Cond', continuousRandom, sep="_"), 'Residual')
   }
   
+  # Subset metadata to just the variables needed. This minimizes overhead for parallelization
+  MetaDF <- MetaDF[, colnames(MetaDF) %in% c("Sample", variableList)]
+
+  if (any(is.na(MetaDF))) {
+    stop("NAs are included in the MetaDF. Please remove them and try again.")
+  }
+  
   # Make your clusters for efficient parallelization
   if(numCores > 1){
       cl <- parallel::makeCluster(numCores)
       parallel::clusterEvalQ(cl, {
                 library(glmmTMB)
         })
-        
-      parallel::clusterExport(cl = cl, 
-        varlist = c("continuousFormula", "ziformula", "modelingData", "MetaDF", "individualVarZIGLMM", "nullDF"),
-        envir = environment()
-      )
-
+  
+    parallel::clusterExport(
+    cl = cl, varlist = c("continuousFormula", "ziformula", "modelingData", "MetaDF", "individualVarZIGLMM", "nullDF"),
+    envir = environment()
+  )
+      
   }else{
 
     cl = NULL
   }
+<<<<<<< Updated upstream
   varDecompList <- pbapply::pblapply(cl = cl, X = rownames(modelingData), individualVarZIGLMM)
 
   if(!is.null(cl)){
     parallel::stopCluster(cl)
+=======
+            
+  if (verbose) {
+    message("Calculating variance....")
+>>>>>>> Stashed changes
   }
-
-
+  
+  varDecompList <- pbapply::pblapply(cl = cl, X = rownames(modelingData), individualVarZIGLMM)
+  parallel::stopCluster(cl)
+                                    
   results <- do.call('rbind', varDecompList)
   rownames(results) <- rownames(modelingData)
 
   return(results)
 }
+
 
 #' @title \code{individualVarZIGLMM }
 #'
@@ -98,19 +145,23 @@ varZIGLMM <- function(TSAM_Object,
 #'
 #' @return A linear model
 
-#' @export
+#' @noRd
 #'
 #'
-individualVarZIGLMM <- function(x) {
-  df <- data.frame(
-    exp = as.numeric(modelingData[x, ]),
-    MetaDF, stringsAsFactors = FALSE
-  )
 
+individualVarZIGLMM <- function(x) {
+    
+    
+    df <- data.frame(
+        exp = as.numeric(modelingData[x, ]),
+        MetaDF, stringsAsFactors = FALSE
+      )
+
+    
   output_vector <- tryCatch(
     {
-      modelRes <- glmmTMB::glmmTMB(continuousFormula,
-        ziformula = ziformula,
+      modelRes <- glmmTMB::glmmTMB(as.formula(continuousFormula),
+        ziformula = as.formula(ziformula),
         data = df,
         family = stats::gaussian(),
         REML = TRUE
