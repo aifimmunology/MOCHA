f099598f87cbefdabae66a2dad4b3e98c7a6bc81
Mark-Phillip Pebworth <markphillip.pebworth@alleninstitute.org>
2023-05-09 11:45:15 -0700

    Revert to older combineSampleTileMatrix

--- R/combineSampleTileMatrix.R
+++ R/combineSampleTileMatrix.R
@@ -1,31 +1,31 @@
 
-#' @title \code{combinePseudobulkSE}
+#' @title \code{combineSampleTileMatrix}
 #'
-#' @description \code{combinePseuduoulkSE} combines all celltypes in a
-#'   PseudobulkSE objects (SampleTileMatrix or pseodobulked scRNA) into a SummarizedExperiment with one single matrix
+#' @description \code{combineSampleTileMatrix} combines all celltypes in a
+#'   SampleTileMatrix object into a SummarizedExperiment with one single matrix
 #'   across all cell types and samples, annotating GC bias using
 #'   chromVAR.
 #'
-#' @param SummarizedExp The SummarizedExperiment object output from
-#'   getSampleTileMatrix or makePseudobulkSE contained cell-type-specific matrices.
+#' @param SampleTileObj The SummarizedExperiment object output from
+#'   getSampleTileMatrix containing your sample-tile matrices
 #' @param NAToZero Set NA values in the sample-tile matrix to zero
 #' @param verbose Set TRUE to display additional messages. Default is FALSE.
 #' @return TileCorr A data.table correlation matrix
 #'
 #'
 #' @export
-combinePseudobulkSE <- function(SummarizedExp,
+combineSampleTileMatrix <- function(SampleTileObj,
                                     NAtoZero = TRUE, 
                                     verbose = FALSE) {
   
-  genome <- S4Vectors::metadata(SummarizedExp)$Genome
+  genome <- S4Vectors::metadata(SampleTileObj)$Genome
   genome <- BSgenome::getBSgenome(genome)
 
   Sample <- Freq <- . <- NULL
   # Extract all the Sample-Tile Matrices for each cell type
-  assays <- SummarizedExperiment::assays(SummarizedExp)
+  assays <- SummarizedExperiment::assays(SampleTileObj)
 
-  coldata <- SummarizedExperiment::colData(SummarizedExp)
+  coldata <- SummarizedExperiment::colData(SampleTileObj)
   
   # Let's generate a new assay, that will contain the
   # the intensity for a given cell, as well as the
@@ -33,7 +33,7 @@ combinePseudobulkSE <- function(SummarizedExp,
 
   newAssays <- list(do.call("cbind", as(assays, "list")))
   newSamplesNames <- unlist(lapply(names(assays), function(x) {
-    gsub(" ", "_", paste(x, colnames(SummarizedExp), sep = "__"))
+    gsub(" ", "_", paste(x, colnames(SampleTileObj), sep = "__"))
   }))
 
   names(newAssays) <- "counts"
@@ -51,29 +51,18 @@ combinePseudobulkSE <- function(SummarizedExp,
     tmp_meta
   }))
   
-  if(all(names(S4Vectors::metadata(SummarizedExp)) %in% FragmentCounts)){
-  
-    cellTypeLabelList <- Var1 <- NULL
-    cellCounts <- as.data.frame(S4Vectors::metadata(SummarizedExp)$CellCounts)
-    cellCounts <- dplyr::mutate(cellCounts,
-        Sample = gsub(" ", "_", paste(cellTypeLabelList, Var1, sep = "__")))
-    cellCounts <- dplyr::select(cellCounts, Sample, Freq)
+  cellTypeLabelList <- Var1 <- NULL
+  cellCounts <- as.data.frame(S4Vectors::metadata(SampleTileObj)$CellCounts)
+  cellCounts <- dplyr::mutate(cellCounts,
+      Sample = gsub(" ", "_", paste(cellTypeLabelList, Var1, sep = "__")))
+  cellCounts <- dplyr::select(cellCounts, Sample, Freq)
 
-    fragCounts <- as.data.frame(S4Vectors::metadata(SummarizedExp)$FragmentCounts)
-    fragCounts <-  dplyr::select(fragCounts, dplyr::one_of(names(assays))) 
-    fragCounts <-  dplyr::mutate(fragCounts, Sample = rownames(fragCounts))
-    fragCounts <-  tidyr::pivot_longer(fragCounts, cols = names(assays), names_to = 'CellTypes', values_to = 'FragNumber') 
-    fragCounts <-  dplyr::mutate(fragCounts, Sample = gsub(" ", "_", paste(CellTypes, Sample, sep = "__")))
-    fragCounts <-  dplyr::select(fragCounts, Sample, FragNumber)
-  }else{
-
-    cellCounts <- as.data.frame(S4Vectors::metadata(SummarizedExp)$CellCounts)
-    cellCounts <- dplyr::mutate(cellCounts,
-        Sample = gsub(" ", "_", paste(cellTypeLabelList, Var1, sep = "__")))
-    cellCounts <- dplyr::select(cellCounts, Sample, Freq)
-    colnames(cellCounts) <- c('Sample','CellCounts')
-
-  }
+  fragCounts <- as.data.frame(S4Vectors::metadata(SampleTileObj)$FragmentCounts)
+  fragCounts <-  dplyr::select(fragCounts, dplyr::one_of(names(assays))) 
+  fragCounts <-  dplyr::mutate(fragCounts, Sample = rownames(fragCounts))
+  fragCounts <-  tidyr::pivot_longer(fragCounts, cols = names(assays), names_to = 'CellTypes', values_to = 'FragNumber') 
+  fragCounts <-  dplyr::mutate(fragCounts, Sample = gsub(" ", "_", paste(CellTypes, Sample, sep = "__")))
+  fragCounts <-  dplyr::select(fragCounts, Sample, FragNumber)
 
   allSampleData <- dplyr::left_join(
     as.data.frame(allSampleData), cellCounts, by = "Sample")
@@ -82,34 +71,28 @@ combinePseudobulkSE <- function(SummarizedExp,
     allSampleData,  fragCounts, by = 'Sample'
   )
 
-  allRanges <- SummarizedExperiment::rowRanges(SummarizedExp)
+  allRanges <- SummarizedExperiment::rowRanges(SampleTileObj)
   for (i in names(assays)) {
     GenomicRanges::mcols(allRanges)[, i] <- rep(TRUE, length(allRanges))
   }
 
   newObj <- SummarizedExperiment::SummarizedExperiment(
-      assays = newAssays,
-      colData = allSampleData,
-      rowRanges = allRanges,
-      metadata = S4Vectors::metadata(SummarizedExp)
-    )
-
-  if(all(names(S4Vectors::metadata(SummarizedExp)) %in% FragmentCounts)){
-
-    
-    newObj <- chromVAR::addGCBias(newObj, genome = genome)
-    
-    if (any(is.na(SummarizedExperiment::rowData(newObj)$bias))) {
-      naList <- is.na(SummarizedExperiment::rowData(newObj)$bias)
-      
-      if (verbose) {
-        warning(paste(sum(naList), "NaNs found within GC Bias", sep = " "))
-      }
-      
-      SummarizedExperiment::rowData(newObj)$bias[which(naList)] <- mean(rowData(newObj)$bias, na.rm = TRUE)
-    }
-  }else{
+    assays = newAssays,
+    colData = allSampleData,
+    rowRanges = allRanges,
+    metadata = S4Vectors::metadata(SampleTileObj)
+  )
   
+  newObj <- chromVAR::addGCBias(newObj, genome = genome)
+  
+  if (any(is.na(SummarizedExperiment::rowData(newObj)$bias))) {
+    naList <- is.na(SummarizedExperiment::rowData(newObj)$bias)
+    
+    if (verbose) {
+      warning(paste(sum(naList), "NaNs found within GC Bias", sep = " "))
+    }
+    
+    SummarizedExperiment::rowData(newObj)$bias[which(naList)] <- mean(rowData(newObj)$bias, na.rm = TRUE)
   }
 
   return(newObj)
