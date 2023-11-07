cb490bb1f2337d386573fab33b569d75f9bf0d3f
Imran McGrath <imran.mcgrath@alleninstitute.org>
2023-11-07 01:40:52 -0800

    test case for exportCoverage

--- tests/testthat/test_exportCoverage.R
+++ tests/testthat/test_exportCoverage.R
@@ -16,15 +16,34 @@ test_that("exportCoverage works on a 3 sample test dataset", {
       MOCHA::exportCoverage(
         SampleTileMatrix,
         dir = mytempdir,
-        type = TRUE, # Insertions
+        type = TRUE, # Coverage
         cellPopulations = "ALL",
         groupColumn = NULL,
         subGroups = NULL,
         sampleSpecific = FALSE,
         saveFile = TRUE,
         numCores = 1,
-        verbose = FALSE)
-    ))
+        verbose = FALSE
+      )
+    )
   )
-
+  
+  capture.output(
+    suppressWarnings(
+      outGR <- MOCHA::exportCoverage(
+        SampleTileMatrix,
+        dir = mytempdir,
+        type = TRUE, # Coverage
+        cellPopulations = "ALL",
+        groupColumn = NULL,
+        subGroups = NULL,
+        sampleSpecific = FALSE,
+        saveFile = FALSE,
+        numCores = 1,
+        verbose = FALSE
+      )
+    )
+  )
+  
+  expect_equal(names(outGR), names(MOCHA:::testTileResultsMultisample)) 
 })
