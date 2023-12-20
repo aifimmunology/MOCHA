3368289c9d94d2d4b2a6a4cf558b66c78feb6c07
Imran McGrath <imran.mcgrath@alleninstitute.org>
2023-12-20 15:48:21 -0800

    update tests and snapshots to include new version number & making package 'purrr' optional

--- tests/testthat/_snaps/list_metadata/callOpenTiles.md
+++ tests/testthat/_snaps/list_metadata/callOpenTiles.md
@@ -100,7 +100,7 @@
       
       $History
       $History[[1]]
-      [1] "callOpenTiles 1.0.0"
+      [1] "callOpenTiles 1.0.2"
       
       
 
---
+++ tests/testthat/test_testCoAccessibilityRandom.R
@@ -0,0 +1,80 @@
+test_that("testCoAccessibleLinks works on a 1 sample test dataset", {
+  cellPopulations <- c("C2", "C5")
+  capture.output(
+    SampleTileMatrix <- MOCHA::getSampleTileMatrix(
+      MOCHA:::testTileResults,
+      cellPopulations = cellPopulations,
+      threshold = 0
+    )
+  )
+
+  cellPopulation <- "C2"
+  regions <- MOCHA::StringsToGRanges(c(
+    "chr1:101873000-101873499"
+  ))
+  capture.output(
+    links <- MOCHA::getCoAccessibleLinks(SampleTileMatrix,
+      cellPopulation,
+      regions,
+      verbose = FALSE
+    )
+  )
+
+  capture.output(
+    results <- MOCHA::testCoAccessibilityRandom(
+      SampleTileMatrix,
+      tile1 = links$Tile1,
+      tile2 = links$Tile2,
+      numCores = 1,
+      ZI = TRUE,
+      backNumber = 1000,
+      verbose = FALSE
+    )
+  )
+
+  expect_snapshot(
+    results,
+    variant = "1sample"
+  )
+})
+
+test_that("testCoAccessibleLinks works on a 3 sample test dataset", {
+  cellPopulations <- c("C2", "C3")
+  capture.output(
+    SampleTileMatrix <- MOCHA::getSampleTileMatrix(
+      MOCHA:::testTileResultsMultisample,
+      cellPopulations = cellPopulations,
+      threshold = 0
+    )
+  )
+
+  cellPopulation <- "C2"
+  regions <- MOCHA::StringsToGRanges(c(
+    "chr1:101775000-101775499"
+  ))
+
+  capture.output(
+    links <- MOCHA::getCoAccessibleLinks(SampleTileMatrix,
+      cellPopulation,
+      regions,
+      verbose = FALSE
+    )
+  )
+
+  capture.output(
+    results <- MOCHA::testCoAccessibilityRandom(
+      SampleTileMatrix,
+      tile1 = links$Tile1,
+      tile2 = links$Tile2,
+      numCores = 1,
+      ZI = TRUE,
+      backNumber = 1000,
+      verbose = FALSE
+    )
+  )
+
+  expect_snapshot(
+    results,
+    variant = "3sample"
+  )
+})
